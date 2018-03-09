97d886fd839fbcafee7c4e8fc0b0c18dc2c90fed
Nelu Dascal <idascal@computervoice.ro>
2018-03-09 08:54:06 +0200

    Added Comments in File var1subII_p1.fs

--- var1subII_p1.fs
+++ var1subII_p1.fs
@@ -1,9 +1,9 @@
 (*
-    Se  considera o  coada în care initial  au  fost  introduse,  în  aceasta
+    Se  considera o  coada in care initial  au  fost  introduse,  in  aceasta
   ordine,  elementele  cu  valorile 1 si  2. Se noteaza cu AD(x) operatia 
   prin care se adauga elementul cu valoarea x in  coada si  cu  EL operatia  
-  prin  care  se  elimina un  element  din  coada.  Câte elemente va contine     
-  coada în urma executarii secventei de operatii: AD(4);EL;EL;AD(5);EL;AD(3) ?
+  prin  care  se  elimina un  element  din  coada.  Cate elemente va contine     
+  coada in urma executarii secventei de operatii: AD(4);EL;EL;AD(5);EL;AD(3) ?
  *)
 open System.Collections.Generic
 let coada=new Queue<int>()
