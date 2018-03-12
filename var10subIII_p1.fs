ac4e012b02402850a5086994fb231a79b807374c
Nelu Dascal <idascal@computervoice.ro>
2018-03-12 10:27:36 +0200

    Added comments in file var10subIII_p1.fs

--- var10subIII_p1.fs
+++ var10subIII_p1.fs
@@ -1,3 +1,16 @@
+(*
+    Implement in F# next function :
+int f(int n,int y) 
+{ 
+    if(n!=0) 
+    { 
+        y=y+1; 
+        return y+f(n-1,y);  
+    } 
+    else 
+        return 0; 
+}   
+*)
 let rec f n y=
     match n with
     | 0 -> 0
