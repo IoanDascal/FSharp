bbb3503de19643a8121a71ad87960b68bd91f469
Nelu Dascal <idascal@computervoice.ro>
2018-03-12 10:13:03 +0200

    Added comments in file backtrackingCartezian.fs

--- backtrackingCartezian.fs
+++ backtrackingCartezian.fs
@@ -1,25 +1,29 @@
+(*
+    Computes the cartesian product of k sets.
+    Each set contains n elements.
+*)
 open System
-printf "Dati n="
-let n=int32(System.Console.ReadLine())
-printf "Dati k="
-let k=int32(System.Console.ReadLine())
+printf "n="
+let n=int32(Console.ReadLine())
+printf "k="
+let k=int32(Console.ReadLine())
 let v=Array.create 10 0
-let validare p=
+let validatePartialSolution p=
     true
 
-let solutie p=
+let completeSolution p=
     p=k
 
-let afisare p=
+let printSolution p=
     for i in 1..p do
         printf "%i" v.[i]
     printfn "" 
 let rec backtracking p=
     for i in 1..n do
         v.[p] <- i
-        if (validare p) then
-            if (solutie p) then
-                afisare p
+        if (validatePartialSolution p) then
+            if (completeSolution p) then
+                printSolution p
                 else
                     backtracking (p+1)
 
