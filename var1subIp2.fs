99774cfd6b1afeeb3475274b3f2aa3b700784b98
Nelu Dascal <idascal@computervoice.ro>
2018-03-12 09:50:42 +0200

    Modified var1subIp2.fs

--- var1subIp2.fs
+++ var1subIp2.fs
@@ -1,30 +1,28 @@
 (*
-Scrieti programul C/C++ corespunzator algoritmului dat. 
-
-citeste n (numar natural)
+read n ( natural number)
 z <- 0  
 p <- 1 
-┌ cat timp n>0 executa
+┌ while n>0 do
 │  c <- n%10  
 │  n <- [n/10] 
-│┌ daca c%3=0 atunci 
+│┌ if c%3=0 then 
 ││   z <- z+p*(9-c) 
 ││   p <- p*10 
 │└■
 └■
-scrie z  
+write z  
 *)
 open System
-printf "Dati un numar intreg n="
+printf " n="
 let n=int32(Console.ReadLine())
 printfn "n =%i" n
 
-let rec sumCifre z n p=
+let rec whileLoop z n p=
     match n with
     | 0 -> z
     |_ -> match (n%10)%3=0 with
-          | true -> sumCifre (z+p*(9-n%10)) (n/10) (p*10)
-          | false -> sumCifre z (n/10) p
+          | true -> whileLoop (z+p*(9-n%10)) (n/10) (p*10)
+          | false -> whileLoop z (n/10) p
 
-let res=sumCifre 0 n 1
-printfn "Suma cifre=%i" res
+let res=whileLoop 0 n 1
+printfn "Sum of digits =%i" res
