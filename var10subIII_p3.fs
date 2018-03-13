c348241a475d21f50a27dd6f44be2934498e9fce
Nelu Dascal <idascal@computervoice.ro>
2018-03-13 09:31:41 +0200

    Added new comments to BAC var 10 sub III p3

--- var10subIII_p3.fs
+++ var10subIII_p3.fs
@@ -1,23 +1,23 @@
 (*
-    Compute the two biggest distinct prime numbers less or equal with n.
+    Compute the two biggest distinct prime numbers less or equal than n.
 *)
 open System
 printf " n="
 let n=int(Console.ReadLine())
-let lista=[]
-let rec prim nr div=
-    match div with
+let primes=[]
+let rec isPrime number divisor=
+    match divisor with
     | 1 -> true
-    | _ -> match nr%div=0 with
+    | _ -> match number%divisor=0 with
            | true -> false
-           | false -> prim nr (div-1)
-let rec sub n (lista:list<int>)=
-    match lista.Length with
-    | 2 -> lista
-    | _ -> if (prim n (n/2)) then 
-               sub (n-1) (n::lista)
+           | false -> isPrime number (divisor-1)
+let rec generatePrimesList n (primesList : list<int>)=
+    match primesList.Length with
+    | 2 -> primesList
+    | _ -> if (isPrime n (n/2)) then 
+               generatePrimesList (n-1) (n::primesList)
            else
-               sub (n-1) lista
+               generatePrimesList (n-1) primesList
 
-let test=sub n lista
+let test=generatePrimesList n primes
 printfn "%A" (List.rev test )
