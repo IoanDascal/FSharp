b6158e7d0f4406881c057823e7d065a65a201776
Nelu Dascal <idascal@computervoice.ro>
2018-03-13 09:08:58 +0200

    Added comments to BAC var 10 sub III p2

--- var10subIII_p2.fs
+++ var10subIII_p2.fs
@@ -1,28 +1,32 @@
+(*
+    Generate all numbers of five digits using backtracking method.
+    Numbers must have first digit 5, and digits must be in increasing order.
+*)
 open System
-printf "Dati n="
-let n=int(System.Console.ReadLine())
-printf "Dati k="
-let k=int(System.Console.ReadLine())
+printf " n="       //Number of digits = 9
+let n=int(Console.ReadLine())
+printf " k="      //Number of digits in the generated numbers
+let k=int(Console.ReadLine())
 let sol=Array.create 10 0
-let afisare p=
+let printSolution p=
     for i in 1..p do
         printf "%i" sol.[i]
     printfn ""
 
-let validare p=
+let validatePartialSolution p=
     match sol.[1] with
     | 5 -> sol.[p-1]<sol.[p]
     | _ -> false
 
-let solutie p=
+let isCompleteSolution p=
     p=k
 
 let rec backtracking p=
     for i in 1..n do
         sol.[p] <- i
-        if (validare p) then
-            if (solutie p) then
-                afisare p
+        if (validatePartialSolution p) then
+            if (isCompleteSolution p) then
+                printSolution p
             else
                 backtracking (p+1)
 
