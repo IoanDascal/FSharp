7037d88cc103f5280e1dcc2fcdecc7a49b7cdfc5
Nelu Dascal <idascal@computervoice.ro>
2018-03-14 10:31:38 +0200

    Added comments to var 10 sub II p5

--- var10subII_p5.fs
+++ var10subII_p5.fs
@@ -1,14 +1,18 @@
+(*
+    Construct a matrix that contains the squares of the first n*p even numbers
+*)
+
 open System
-printf "Dati n="
-let n=int(System.Console.ReadLine())
-printf "Dati p="
-let p=int(System.Console.ReadLine())
+printf "Number of rows n="
+let n=int(Console.ReadLine())
+printf "Number of columns p="
+let p=int(Console.ReadLine())
 let mutable x=0
-let matrice=[|for i in 0..n-1 do
+let matrix=[|for i in 0..n-1 do
                   yield [|for j in 0..p-1 do 
-                              yield (pown x 2);x<-x+2|]|]
+                              yield (pown x 2);x<-x+2|]|]             
 
 for i in 0..n-1 do
     for j in 0..p-1 do
-        printf "%i " matrice.[i].[j]
+        printf "%8i " matrix.[i].[j]
     printfn ""
