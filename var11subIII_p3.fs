3067fc705b2607f833cc698416e54bc28306badc
Nelu Dascal <idascal@computervoice.ro>
2018-03-15 10:54:52 +0200

    Added comments to var 11 sub III p3

--- var11subIII_p3.fs
+++ var11subIII_p3.fs
@@ -1,18 +1,38 @@
+(*
+    File nrvar113.txt contains in the first row a value for n.
+    In the second row it contains n integer numbers. Write 
+    in position i the biggest between the number in position i from file
+    and the maximum number from position 0 to i-1.
+*)
 open System.IO
-let citireLinie=
-    use input=File.OpenText("nrVar113.txt")
-    let sir=input.ReadToEnd()
-    let sir=sir.Replace(System.Environment.NewLine," ")
-    let sir=sir.Split([|' '|])
-    sir
+let readFile=
+    use input=File.OpenText("FSharp/nrVar113.txt")
+    let inputString=input.ReadToEnd()
+    let inputString=inputString.Replace(System.Environment.NewLine," ")
+    let inputString=inputString.Split([|' '|])
+    inputString
 
-let cit=citireLinie
-let numere= Array.map (fun x -> int(x)) cit
-let n=numere.[0]
+let numbers= Array.map (fun x -> int(x)) readFile
+let n=numbers.[0]
+(*
+         Version 1
+
 printfn "n=%i" n
-let mutable maxim=numere.[1]
+let mutable maxim=numbers.[1]
 for i in 1..n do
-    if maxim < numere.[i] then maxim <- numere.[i]
-                               printf "%i " maxim
+    if maxim < numbers.[i] then maxim <- numbers.[i]
+                                printf "%i " maxim
                             else printf "%i " maxim
-                          
+*)
+//        Version 2
+
+let rec forLoop i maximum=
+    match i<=n with
+    | false -> ()
+    | true -> match numbers.[i] > maximum with
+              | true -> printf "%i " numbers.[i]
+                        forLoop (i+1) numbers.[i]
+              | false -> printf "%i " maximum 
+                         forLoop (i+1) maximum  
+
+forLoop 1 numbers.[1]
