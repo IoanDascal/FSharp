75f0bf94334ddbf3ad90c93f2550682ae703d122
Nelu Dascal <idascal@computervoice.ro>
2018-03-16 14:30:25 +0200

    Added comments to BAC var 12 sub III p3

--- var12subIII_p3.fs
+++ var12subIII_p3.fs
@@ -1,17 +1,27 @@
+(*
+    Each of files nrVar123I.txt and nrVar123II.txt contains an array of 
+    integers in ascending order. Extract from files all numbers that are
+    divisible by "div" and appears only in one file.
+    Example: If nrVar123I.txt contains : 1 2 3 4 7 20 60
+    and nrVar123II.txt contains : 3 5 7 8 9 10 12 20 24
+    then the result is : 5 10 60 , if div=5
+*)
 open System.IO
-let citire numeFisier=
-    use input=File.OpenText(numeFisier)
-    let sir=input.ReadToEnd()
-    let sir=sir.Replace(System.Environment.NewLine," ")
-    let sir=sir.Split([|' '|])
-    sir
+let readFile fileName=
+    use input=File.OpenText(fileName)
+    let inputString=input.ReadToEnd()
+    let inputString=inputString.Replace(System.Environment.NewLine," ")
+    let inputArray=inputString.Split([|' '|])
+    inputArray  
 
 
 
-let lista1=List.ofArray (Array.map (fun x -> int(x)) (citire "nrVar123I.txt"))
-let lista2=List.ofArray (Array.map (fun x -> int(x)) (citire "nrVar123II.txt"))
+let list1=List.ofArray (Array.map (fun x -> int(x)) (readFile "nrVar123I.txt"))
+let list2=List.ofArray (Array.map (fun x -> int(x)) (readFile "nrVar123II.txt"))
+printf "div="
+let div=int32(System.Console.ReadLine())
 
-let merge xs ys=
+let mergeLists xs ys=
     let rec loop xs ys=
         seq{
             match xs,ys with
@@ -27,9 +37,9 @@ let merge xs ys=
         }
     loop xs ys |> List.ofSeq
 
-let final= merge lista1 lista2
-printfn "%A" final
-let res=List.choose (fun x -> match x%5 with
+let finalList= mergeLists list1 list2
+printfn "%A" finalList
+let res=List.choose (fun x -> match x%div with
                               | 0 -> Some(x)
-                              | _ -> None) final
+                              | _ -> None) finalList
 printfn "%A" res
