d8aaa933e50a91f90f10388c0c9d2b16cd78f6ef
Nelu Dascal <idascal@computervoice.ro>
2018-03-16 11:20:48 +0200

    Added comments to BAC var 12 sub II p5

--- var12subII_p5.fs
+++ var12subII_p5.fs
@@ -1,8 +1,16 @@
-open System
-printf "Dati n="
+(*
+    Generate a matrix that contains the last digit of product i*j.
+    n - number of rows and columns of the matrix,
+    i - the row number
+    j - the column number
+*)
+printf " n="
 let n=int(System.Console.ReadLine())
-let matrice=[|for i in 0..n-1 do
+let matrix=[|for i in 0..n-1 do
                   yield ([|for j in 0..n-1 do
                                yield ((i+1)*(j+1)%10)|])|]
 
-printfn "%A" matrice
+for i in 0..n-1 do
+    for j in 0..n-1 do
+        printf "%3i" matrix.[i].[j]
+    printfn ""
