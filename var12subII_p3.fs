ec672fdf4b8c4e8c196ee57845af58b05ea66f06
Nelu Dascal <idascal@computervoice.ro>
2018-03-16 14:34:48 +0200

    Added comments to BAC var 12 sub II p3

--- var12subII_p3.fs
+++ var12subII_p3.fs
@@ -1,15 +1,20 @@
+(*
+    The parents array of a tree with n=9 vertices is :
+    father=(8,7,6,6,7,7,8,0,8), where vertex 8 is the parent of
+    vertex 1, vertex 7 is the parent of vertex 2, etc.
+    a. Which vertex is the root of the tree?
+    b. Which vertices are the childrens if vertex 7
+*)
 open System
-printf "Dati numarul de noduri n="
-let n=int(System.Console.ReadLine())
-let tati=[for i in 1..n do
-              yield (printf "tata[%i]=" i
-                     int(System.Console.ReadLine()))]
-printfn "%A" tati
-let radacina= tati |> List.findIndex (fun x -> x=0)
-printfn "Nodul radacina este= %i" (radacina+1)
-printfn "Descendentii nodului 7 sunt :"
-//tati |> List.iteri (fun i x -> if x=7 then printf "%i " (i+1)
-  //                                 else printf "")
-let descendenti=[for i in 0..n-1 do
-                     if tati.[i]=7 then yield (i+1)]
-printfn "%A" descendenti
+printf "Number of vertices n="
+let n=int(Console.ReadLine())
+let fathers=[for i in 1..n do
+              yield (printf "fathers[%i]=" i
+                     int(Console.ReadLine()))]
+printfn "%A" fathers
+let root= fathers |> List.findIndex (fun x -> x=0)
+printfn "The root of the tree is vertex = %i" (root+1)
+printfn "Childrens of vertex 7 are :"
+let childrens=[for i in 0..n-1 do
+                     if fathers.[i]=7 then yield (i+1)]
+printfn "%A" childrens
