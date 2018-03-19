33518ee59b2c699a772c96b1c3026d67553510f5
Nelu Dascal <idascal@computervoice.ro>
2018-03-19 13:22:29 +0200

    Added comments to BAC var 13 sub III p4

--- var13subIII_p4.fs
+++ var13subIII_p4.fs
@@ -1,34 +1,34 @@
-open System.IO
 (*
-printf "Dati n="
-let n=int(System.Console.ReadLine())
-printf "Dati c="
-let c=int(System.Console.ReadLine())
+    File nrVar134.txt contains an array of integers.
+    Generate a list that contains only the even digits.
+    Example: If the array is : 25 7 38 1030 45127 0 35 60 15
+    then the final list must be : 2 8 42 60
 *)
-let rec eliminaCifra n c nr p=
+open System.IO
+let rec removeDigit n c nr p=
     match n with
     | 0 -> nr
     | _ -> match (n%10)=c with
-           | true -> eliminaCifra (n/10) c nr p
-           | false -> eliminaCifra (n/10) c (nr+(n%10)*p) (p*10)
+           | true -> removeDigit (n/10) c nr p
+           | false -> removeDigit (n/10) c (nr+(n%10)*p) (p*10)
 
-let citireFisier=
+let readFile=
     use input=File.OpenText("nrVar134.txt")
-    let sir=input.ReadToEnd()
-    let sir=sir.Replace(System.Environment.NewLine," ")
-    let sir=sir.Split([|' '|])
-    sir
+    let inputString=input.ReadToEnd()
+    let inputString=inputString.Replace(System.Environment.NewLine," ")
+    let inputArray=inputString.Split([|' '|])
+    inputArray 
 
-let rec eliminaCifre (numar:int) (lst:int list)=
+let rec removeDigits (number:int) (lst:int list)=
     match lst with
-    | [] -> numar
-    | head::tail -> eliminaCifre (eliminaCifra numar head 0 1) tail
-let numere=Array.map (fun x -> int(x)) (citireFisier)
+    | [] -> number
+    | head::tail -> removeDigits (removeDigit number head 0 1) tail
+let numbers=Array.map (fun x -> int(x)) (readFile)
 
-let res=Array.choose (fun x -> let x1=eliminaCifre x [1;3;5;7;9]
+let res=Array.choose (fun x -> let x1=removeDigits x [1;3;5;7;9]
                                match x>=x1 && x1>0 with
                                | true -> Some(x1)
-                               | false -> None) numere
+                               | false -> None) numbers
 printfn "%A" res
 let resString=res |> Array.map (fun x -> string(x)) |> String.concat " "
 printfn "ress=%s" resString
