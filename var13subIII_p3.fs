c27d90be41b5d5ad2559a2bded561ca4a41d4720
Nelu Dascal <idascal@computervoice.ro>
2018-03-19 12:52:21 +0200

    Added comments to BAC var 13 sub III p3

--- var13subIII_p3.fs
+++ var13subIII_p3.fs
@@ -1,12 +1,19 @@
+(*
+    If we consider the series: 1,  2,1,  3,2,1,  4,3,2,1,  ......
+    the first group is 1, the second group is 2,1,  and the k'th group
+    is: k,k-1,k-2,...,1. Determine the term n of the initial series.
+*)
 open System
 printf "Dati n="
-let n=int(System.Console.ReadLine())
-let rec grupa k n acc=
+let n=int(Console.ReadLine())
+let rec kGroup k acc=
     match acc<n with
     | false -> k-1
-    | true -> grupa (k+1) n (acc+k)
+    | true -> kGroup (k+1) (acc+k)
 
-let test= grupa 1 n 0
-let suma=(test*(test+1))/2
-let nrCautat=test-suma+n
-printfn "%i" nrCautat
+let k= kGroup 1 0
+printfn "k=%i" k
+let numberOfElementsBeforeKGroup =(k*(k-1))/2
+let positionOfTermNInGroupK=n-numberOfElementsBeforeKGroup
+let nTerm=k-positionOfTermNInGroupK+1
+printfn "%i" nTerm
