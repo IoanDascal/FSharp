10e5534c8c256774e968b1a975f6dcd8b8221194
Nelu Dascal <idascal@computervoice.ro>
2018-03-20 14:54:11 +0200

    Added comments to BAC var 14 sub III p4

--- var14subIII_p4.fs
+++ var14subIII_p4.fs
@@ -1,20 +1,25 @@
+(*
+    File nrvar144.txt contains an integer number on each line.
+    Print on each row five of them  and compute  how many numbers have the
+    sum of digits  an even number.
+*)
 open System.IO
-let rec sumCifre suma n=
+let rec sumOfDigits sum n=
     match n with
-    | 0 -> suma
-    | _ -> sumCifre (suma+n%10) (n/10)
+    | 0 -> sum
+    | _ -> sumOfDigits (sum+n%10) (n/10)
 let res=
     let mutable i=0
-    let mutable nrSumePare=0
+    let mutable nrEvenSum=0
     use sr=new StreamReader("nrvar144.txt")
     while not sr.EndOfStream do
         let x=int(sr.ReadLine())
         i<-i+1
-        let sumCif=sumCifre 0 x
-        if (sumCif%2)=0 then nrSumePare <- nrSumePare+1
+        let sumDigits=sumOfDigits 0 x
+        if (sumDigits%2)=0 then nrEvenSum <- nrEvenSum+1
             else () 
         printf "%i " x
         if i%5=0 then printfn ""
-    nrSumePare
+    nrEvenSum
 
 printfn "\n %i" res
