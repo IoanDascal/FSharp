f3986992f97d21870a028bc514121cb7bc915f7f
Nelu Dascal <idascal@computervoice.ro>
2018-03-20 13:46:47 +0200

    Added comments to BAC var 14 sub II p5

--- var14subII_p5.fs
+++ var14subII_p5.fs
@@ -1,9 +1,20 @@
-open System
-printf "Dati n="
+(*
+    Given an integer nr >0, with n digits (n<=8), generate a matrix 
+    with n rows and n columns. First column must contain the last digit,
+    second column must contain last but one digit, etc.
+    Example : If nr=1359 the matrix should be:
+       | 9 5 3 1 |
+       | 9 5 3 1 |
+       | 9 5 3 1 |
+       | 9 5 3 1 |
+*)
+printf " n="
 let n=int(System.Console.ReadLine())
 let s= string n
-let linie=[|for i in 0..s.Length-1 do
+let line=[|for i in 0..s.Length-1 do
                 yield (int(s.[s.Length-i-1])-48)|]
-printfn "%A" linie
-let matrice=[|for i in 0..s.Length-1 do yield linie|]
-printfn "%A" matrice
+printfn "%A" line
+printfn ""
+let matrix=[|for i in 0..s.Length-1 do yield line|]
+for line in matrix do
+    printfn "%A" line
