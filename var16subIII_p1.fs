d3f93c1a160cbe0cb8503c9e0ad49b006fc2d210
Nelu Dascal <idascal@computervoice.ro>
2018-03-21 09:45:31 +0200

    Added comments to BAC var 16 sub III p1

--- var16subIII_p1.fs
+++ var16subIII_p1.fs
@@ -1,3 +1,11 @@
+(*
+    Implement in F# next C++ snipet:
+ int F(int n)
+ {
+     if(n==0 || n==1) return 1;
+        else
+            return 2*F(n-1)+2*F(n-2);}
+*)
 let rec f n=
     match n with
     | 0 | 1 -> 1
