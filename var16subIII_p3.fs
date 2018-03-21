e013cfd058018d15fe14792dcbd1c4801df4097c
Nelu Dascal <idascal@computervoice.ro>
2018-03-21 10:10:03 +0200

    Added comments to BAC var 16 sub III p3

--- var16subIII_p3.fs
+++ var16subIII_p3.fs
@@ -1,18 +1,23 @@
+(*
+    Given a list of n integers less than 1000 and a digit k,
+    select all numbers with the last digit equal to k and are
+    divisible with k.
+*)
 open System
-printf "Dati n="
+printf "n="
 let n=Console.ReadLine() |> int
-printf "Dati k="
+printf "k="
 let k=Console.ReadLine() |> int
-printfn "Dati elementele tabloului:"
-let numere=[for i in 0..n-1 do
-                yield (printf "numere[%i]=" i
+printfn "The elements of the list are :"
+let numbers=[for i in 0..n-1 do
+                yield (printf "numbers[%i]=" i
                        int(System.Console.ReadLine())) ] 
 
-printfn "%A" numere
-let valideazaNumar nr k=
+printfn "%A" numbers
+let validateNumber nr k=
     match nr%k with
     | 0 -> nr%10=k
     | _ -> false
 
-let res = List.choose (fun x -> if (valideazaNumar x k) then Some x else None) numere
-printfn "Numerele cautate sunt: %A" res
+let res = List.choose (fun x -> if (validateNumber x k) then Some x else None) numbers
+printfn "Numbers are : %A" res
