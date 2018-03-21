e7b791522cecbefdba4cc5ec66cc2efb3e8ae15a
Nelu Dascal <idascal@computervoice.ro>
2018-03-21 10:24:26 +0200

    Added comments to BAC var 16 sub III p4

--- var16subIII_p4.fs
+++ var16subIII_p4.fs
@@ -1,20 +1,25 @@
+(*
+    File nrVar164.txt contains on each line a natural number
+    with maximum nine digits. Print in decreasing order all digits
+    from all numbers.
+*)
 open System.IO
 let input=
     try
         File.OpenText "nrVar164.txt"
     with
         | :? FileNotFoundException -> printfn "Error: nrVar164.txt not found.";exit(1)
-let cifre=Array.zeroCreate<int> 10
-let res (linie:string)=
-    match linie with
+let digits=Array.zeroCreate<int> 10
+let readLine (line:string)=
+    match line with
     | null -> false
     | sir -> for i in 0..sir.Length-1 do 
-                 cifre.[(int sir.[i])-48] <- cifre.[(int sir.[i])-48]+1
+                 digits.[(int sir.[i])-48] <- digits.[(int sir.[i])-48]+1
              true
 
-while res (input.ReadLine()) do ()
-printfn "%A" cifre 
+while readLine (input.ReadLine()) do ()
+printfn "%A" digits 
 for i in 9.. -1..0 do
-    for j in 0..cifre.[i]-1 do
+    for j in 0..digits.[i]-1 do
         printf "%i" i
 printfn ""
