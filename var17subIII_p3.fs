453b1c70d26bfd7d62a02e531ae76218d35424a1
Nelu Dascal <idascal@computervoice.ro>
2018-03-22 10:35:07 +0200

    Added comments to BAC var 17 sub III p3

--- var17subIII_p3.fs
+++ var17subIII_p3.fs
@@ -1,12 +1,16 @@
+(*
+    Given a list of n integers, calculate how many numbers are in the range
+    formed by the first and the last item from the list.
+*)
 open System
-printf "Dati n="
-let n=int(System.Console.ReadLine())
-let vector=[for i in 1..n do yield int(System.Console.ReadLine())]
+printf "n="
+let n=int(Console.ReadLine())
+let vector=[for i in 1..n do yield int(Console.ReadLine())]
 let a=vector.[0]
 let b=vector.[n-1]
 let swap (x,y)=(y,x)
-let interschimba=if a>b then swap (a,b) else (a,b)
-let mic=fst interschimba
-let mare=snd interschimba
-let nr=List.countBy (fun x -> x>=mic && x<=mare) vector
-printfn "Sunt %i numere intre %i si %i." (snd nr.[0]) mic mare
+let interchange=if a>b then swap (a,b) else (a,b)
+let low=fst interchange
+let big=snd interchange
+let nr=List.countBy (fun x -> x>=low && x<=big) vector
+printfn "There are %i numbers between %i si %i." (snd nr.[0]) low big
