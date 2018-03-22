ce4ce0a899153037766add54145545390e552c62
Nelu Dascal <idascal@computervoice.ro>
2018-03-22 10:23:43 +0200

    Added comments to BAC var 17 sub III p2

--- var17subIII_p2.fs
+++ var17subIII_p2.fs
@@ -1,26 +1,30 @@
+(*
+    Generate all numbers of k digits from n digits.
+    The digits from each number must be in increasing order.
+*)
 open System
-printf "Dati n="
+printf "n="
 let n=Console.ReadLine() |> int
-printf "Dati k="
-let k=int(System.Console.ReadLine())
-let sol=Array.zeroCreate<int> 10
-let valid p=
+printf "k="
+let k=int(Console.ReadLine())
+let solution=Array.zeroCreate<int> 10
+let validatePartialSolution p=
     match p with 
     | 1 -> true
-    | _ -> sol.[p]>sol.[p-1]
-let solutie p=
+    | _ -> solution.[p]>solution.[p-1]
+let isCompleteSolution p=
     p=k
 
-let afisare p=
+let printSolution p=
     for i in 1..p do
-        printf "%i" sol.[i]
+        printf "%i" solution.[i]
     printfn ""
 let rec backtracking p=
     for i in 1..n do
-        sol.[p] <- i
-        if valid p then
-            if solutie p then
-                afisare p
+        solution.[p] <- i
+        if validatePartialSolution p then
+            if isCompleteSolution p then
+                printSolution p
                 else 
                     backtracking (p+1)
 let res=backtracking 1
