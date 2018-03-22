93e172076ca0163c8af07e87a304a339dc67015f
Nelu Dascal <idascal@computervoice.ro>
2018-03-22 09:31:54 +0200

    Added BAC var 17 sub I p2

--- var17subI_p2.fs
+++ var17subI_p2.fs
@@ -1,18 +1,31 @@
+(*
+ read x,y  ( natural numbers)
+┌ if x<y then
+│     x <- x-y
+│     y <- x+y
+│     x <-y-x
+└■
+┌ while x≥y do
+│     write ’A’
+│     x <- x-y
+│     write ’B’
+└■
+*)
 open System
-printf "Dati x="
-let x=int(System.Console.ReadLine())
-printf "Dati y="
-let y=int(System.Console.ReadLine())
+printf " x="
+let x=int(Console.ReadLine())
+printf " y="
+let y=int(Console.ReadLine())
 let swap (y,x)=(x,y)
-let interschimbare=if x<y then swap (x,y) else (x,y)
-printfn "%i" (fst interschimbare)
-let a=fst interschimbare
-let b=snd interschimbare
-let rec afisare a b=
+let interchange=if x<y then swap (x,y) else (x,y)
+printfn "%i" (fst interchange)
+let a=fst interchange
+let b=snd interchange
+let rec printResult a b=
     match a>=b with
     | false -> printf ""
     | true -> printf "A"
-              afisare (a-b) b
+              printResult (a-b) b
               printf "B"
 
-let res = afisare a b
+let res = printResult a b
