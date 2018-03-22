434f37fa52361ff9b11508bac4a70af73caa6967
Nelu Dascal <idascal@computervoice.ro>
2018-03-22 10:11:01 +0200

    Added comments to BAC var 17 sub III p1

--- var17subIII_p1.fs
+++ var17subIII_p1.fs
@@ -1,3 +1,14 @@
+(*
+    Implement in F# next C++ function :
+ void f(long n)
+ {
+     if (n>9)
+     {
+         cout<<n/100;
+         f(n/10);
+     }
+ } 
+*)
 let rec f n=
     match n>9 with
     | false -> ()
