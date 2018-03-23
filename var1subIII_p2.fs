063b13bea9fd306ecd7e2cfaa742a268f610ecec
Nelu Dascal <idascal@computervoice.ro>
2018-03-23 09:46:24 +0200

    Added comments BAC var 1 sub III p2

--- var1subIII_p2.fs
+++ var1subIII_p2.fs
@@ -1,9 +1,21 @@
+(*
+    Implement in F# next C++ function:
+void f(long n, int i)
+{ 
+    if(n!=0)
+    if(n%3>0)
+    { 
+        cout<<i; 
+        f(n/3,i+1); 
+    }
+}
+*)
 let rec f n i =
     match n with
-    | 0 -> None
+    | 0 -> ()
     | _ ->  match n%3 with
-            | 0 -> None |> ignore
-            | _ -> Some( printf "%i" i) |>ignore
-            f (n/3) (i+1)
+            | 0 -> () 
+            | _ -> printf "%i" i
+                   f (n/3) (i+1)
     
-let res:obj option=f 121 1
+let res=f 121 1
