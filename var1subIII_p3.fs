4c6a7467847e9bed5685869cdba99c6f9060ee59
Nelu Dascal <idascal@computervoice.ro>
2018-03-23 10:08:22 +0200

    Added comments BAC var 1 sub III p3

--- var1subIII_p3.fs
+++ var1subIII_p3.fs
@@ -1,26 +1,26 @@
-//Read from file
-open System.Net
+(*
+    File test.txt contains an array of integer numbers.
+    Write a program that reads the numbers from the file and
+    print only the numbers divisible by n.
+*)
 open System.IO
+let readFile=
+    use input=File.OpenText("nrVar13.txt")
+    let inputString=input.ReadToEnd()
+    let inputString=inputString.Replace(System.Environment.NewLine," ")
+    let inputArray=inputString.Split([|' '|])
+    inputArray
 
-//let strip chars=String.map (fun c -> if Seq.exists((=)c) chars then ' ' else c)
-
-let citire=
-    use input=File.OpenText("test.txt")
-    let str=input.ReadToEnd()
-    let str=str.Replace(System.Environment.NewLine," ")
-    printfn "str=%s" str
-    let res=str.Split([|' '|])
-    res
-
-let sir=citire 
-let lung=sir.Length
-printfn "%d" lung
-printfn "%A" sir
-printf "Dati un numar n="
+let array=readFile 
+printf " n="
 let n=int32(System.Console.ReadLine())
 printfn "n=%d" n
 
-let numere=sir.[..(lung-2)] |> Array.map System.Int32.Parse
-                            |> Array.filter (fun x -> x%n=0)
-for num in numere do
-    printf "%i  " num
+let numbers=array.[..(array.Length-1)] |> Array.map System.Int32.Parse
+                                       |> Array.filter (fun x -> x%n=0)
+if numbers.Length=0 then
+    printfn "The file does not contain any number divisible by n"
+else
+    for number in numbers do
+        printf "%i  " number
+printfn ""
