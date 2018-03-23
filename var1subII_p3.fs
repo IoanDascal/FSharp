4c34c036b13f1cb2572d1da8d71b5cc666e6e79a
Nelu Dascal <idascal@computervoice.ro>
2018-03-23 09:16:34 +0200

    Added comments BAC var 1 sub II p3

--- var1subII_p3.fs
+++ var1subII_p3.fs
@@ -1,23 +1,27 @@
+(*
+    p1 and p2 are end points of a line segment, and p3 is the midpoint
+    of the segment. Calculate the coordinates x and y of the midpoint p3.
+*)
 open System
-type Punct=
+type Point=
     {
         x:float;
         y:float;
     }
 
-let citeste msg:string=
+let readValue msg:string=
     printf "%s" msg
-    let a=System.Console.ReadLine()
+    let a=Console.ReadLine()
     a
 
-let a=float(citeste "Dati a=")
+let a=float(readValue " a=")
 printfn "%f" a
-let b=float(citeste "Dati b=")
+let b=float(readValue " b=")
 printfn "%f" b
 let p1={x=a;y=b}
-let c=float(citeste "Dati c=")
+let c=float(readValue " c=")
 printfn "%f" c
-let d=float(citeste "Dati d=")
+let d=float(readValue " d=")
 printfn "%f" d
 let p2={x=c;y=d}
 let p3={x=(p1.x+p2.x)/2.0;y=(p1.y+p2.y)/2.0}
