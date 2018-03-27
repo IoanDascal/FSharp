e2e5c6353b81efdba709dbb51359dacf262d637c
Nelu Dascal <idascal@computervoice.ro>
2018-03-27 09:51:09 +0300

    Added comments to BAC var 18 sub I p2

--- var18subI_p2.fs
+++ var18subI_p2.fs
@@ -1,15 +1,28 @@
+(*
+read x,y  ( natural numbers) 
+┌ if x>y then
+│    y <--> x
+└■
+┌ if x%2=0 then
+│    x <- x+1 
+└■
+┌ while x ≤ y do
+│    x < x+2 
+│    write ‘*’ 
+└■
+*)
 open System
-printf "Dati x="
+printf " x="
 let x=Console.ReadLine() |> int
-printf "Dati y="
-let y=int(System.Console.ReadLine())
+printf " y="
+let y=int(Console.ReadLine())
 let swap (x,y)=(y,x)
-let interschimbare=if x>y then swap (x,y) else (x,y)
-let a=if (fst interschimbare)%2=0 then ((fst interschimbare)+1 ) else (fst interschimbare)
-let rec afisare x y=
+let interchange=if x>y then swap (x,y) else (x,y)
+let a=if (fst interchange)%2=0 then ((fst interchange)+1 ) else (fst interchange)
+let rec whileLoop x y=
     match x<=y with
     | false -> printfn ""
     | true -> printf "*"
-              afisare (x+2) y
+              whileLoop (x+2) y
 
-let res=afisare a (snd interschimbare)
+let res=whileLoop a (snd interchange)
