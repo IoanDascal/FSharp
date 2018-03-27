b28bbc1bb454f0af315b8a21e02da96af7b2f064
Nelu Dascal <idascal@computervoice.ro>
2018-03-27 10:00:02 +0300

    Added comments to BAC var 87 sub I p2

--- var87subI_p2.fs
+++ var87subI_p2.fs
@@ -31,10 +31,6 @@ let rec whileLoop a b c=
     match a<>b || a<>c with
     |false ->  a
     | true -> let x=if (a>b && a>c) then c else if (a>b && a<=c) then b else if (a<b && a>c) then c else a 
-              //let aVal=if x<>a then a-x else a
-              //let bVal=if x<>b then b-x else b
-              //let cVal= if x<>c then c-x else c
-              printfn "%i %i %i" a b c
               whileLoop (if x<>a then a-x else a) (if x<>b then b-x else b) (if x<>c then c-x else c)
 
 let res=whileLoop a b c  
