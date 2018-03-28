a48fc3380dcf0503d47a90e7b20f88432641d6cc
Nelu Dascal <idascal@computervoice.ro>
2018-03-28 09:01:23 +0300

    Added comments to BAC var 15 sub I p2

--- var15subI_p2.fs
+++ var15subI_p2.fs
@@ -1,14 +1,25 @@
+(*
+ read a,n  ( natural numbers)
+┌ for i <- 1,n do
+│ ┌ if i%2=0 then
+│ │     a <- a-i*i
+│ │ else
+│ │     a <- a+i*i
+│ └■
+└■
+ write a 
+*)
 open System
-printf "Dati a="
-let a=int(System.Console.ReadLine())
-printf "Dati n="
-let n=int(System.Console.ReadLine())
-let rec calcul a n=
+printf " a="
+let a=uint32(Console.ReadLine())
+printf " n="
+let n=uint32(Console.ReadLine())
+let rec forLoop (a:uint32) (n:uint32)=
     match n with
-    | 1 -> a
-    | _ -> match i%2=0 with
-           | true -> calcul (a-n*n) (n-1)
-           | false -> calcul (a+n*n) (n-1)
+    | 0u -> a
+    | _ -> match n%2u = 0u with
+           | true -> forLoop (a-n*n) (n-1u)
+           | false -> forLoop (a+n*n) (n-1u)
 
-let res=calcul a n
+let res=forLoop a n
 printfn "%i" res
