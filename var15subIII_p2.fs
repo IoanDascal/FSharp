63ede72f69f3d733b965f5b04af904fd3c56195f
Nelu Dascal <idascal@computervoice.ro>
2018-03-29 09:17:18 +0300

    Added comments to BAC var 15 sub III p2

--- var15subIII_p2.fs
+++ var15subIII_p2.fs
@@ -1,12 +1,33 @@
+(*
+    Implement in F# next C++ snipet:
+ long g(long x)
+ {
+      if (x>9)
+           return (x/10 + x%10);
+      else
+         return x;
+ } 
+ long f(int c)
+ { 
+     if (c<1)
+         return 1;
+     else
+        return g(c+f(c-1));
+ }
+*)
 let g x=
-    if x>9 then (x/10+x%10) else x
+    if x>9  then 
+        (x/10+x%10) 
+    else 
+        x
 
 let rec f c=
-    if c<1 then 1
-        else
-            g (c+f (c-1))
+    if c<1 then
+        1
+    else
+        g (c+f (c-1))
 
-let res = f 11
+let res = g 11
 printfn "%i" res
-let res1 = f 11
+let res1 = f 6
 printfn "%i" res1
