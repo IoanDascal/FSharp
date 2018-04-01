56ddaf1a7eef582c5626311cd94bfe803fdabc22
Ioan Dascal <profuinfo2000@gmail.com>
2018-04-01 14:28:05 +0300

    Added BAC var 77 sub I p 2

---
+++ var77subI_p2.fs
@@ -0,0 +1,28 @@
+(*
+ read a  (natural number )
+ k <- 0 
+┌ while a ≠ 0 do
+│  read b  (natural number )
+│┌ if a < b then
+││    k <- k+1 
+│└■
+│  a <- b 
+└■
+ write k 
+*)
+
+printf "a="
+let a=int(System.Console.ReadLine())
+let rec whileLoop a k=
+    match a<>0 with
+    | false -> k
+    | true -> printf "b="
+              let b=int(System.Console.ReadLine())
+              match a<b with
+              | true -> whileLoop b (k+1)
+              | false -> whileLoop b k
+
+let k=whileLoop a 0
+printfn "%i" k
+
+
