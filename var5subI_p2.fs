cf0bfe4efddbcdfdfcd125777336d57043302b4b
Nelu Dascal <idascal@computervoice.ro>
2018-04-02 09:55:59 +0300

    Added comments to BAC var 5 sub I pr 2

--- var5subI_p2.fs
+++ var5subI_p2.fs
@@ -1,26 +1,43 @@
+(*
+  read x,z ( natural numbers)
+  y <- 0
+┌ repeat
+│    y <- y*10+x%10
+│    x <- [x/100]
+└ until x=0
+┌ while y*z>0 and y%10=z%10 do
+│    y <- [y/10]
+│    z <- [z/10]
+└■
+┌ if y+z=0 then
+│    write 1
+│ else
+│    write 0
+└■
+*)
 open System
-printf "Dati x="
-let x=int32(System.Console.ReadLine())
-printf "Dati z="
-let z=int32(System.Console.ReadLine())
-let rec calcul y x=
+printf "Enter x="
+let x=int32(Console.ReadLine())
+printf "Enter z="
+let z=int32(Console.ReadLine())
+let rec repeatLoop y x=
     match x with
     | 0 -> y
-    | _ -> calcul (y*10+x%10) (x/100)
+    | _ -> repeatLoop (y*10+x%10) (x/100)
 
-let rec calculYZ (f,s )=
-    match f*s>0 && f%10=s%10 with
-    | false -> (f,s)
-    | true -> calculYZ (f/10,s/10)
+let rec whileLoop (y,z)=
+    match y*z>0 && y%10=z%10 with
+    | false -> (y,z)
+    | true -> whileLoop (y/10,z/10)
 
-let scrie (f,s)=
+let writeResult (f,s)=
     match f+s with
     | 0 -> printfn "1"
     | _ -> printfn "0"
 
-let y=calcul 0 x
+let y=repeatLoop 0 x
 printfn "y=%i" y
-let rez=calculYZ (y, z)
-printfn "y=%i" (fst rez)
-printfn "z=%i" (snd rez)
-let final=scrie rez
+let res=whileLoop (y, z)
+printfn "y=%i" (fst res)
+printfn "z=%i" (snd res)
+let final=writeResult res 
