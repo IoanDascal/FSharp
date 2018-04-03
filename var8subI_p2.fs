fc2c985d41c5b33e6cd0fc025f69c168a3f36e13
Nelu Dascal <idascal@computervoice.ro>
2018-04-03 10:09:39 +0300

    Added comments to BAC var 8 sub I p2

--- var8subI_p2.fs
+++ var8subI_p2.fs
@@ -1,14 +1,29 @@
+(*
+ read n,k ( natural numbers n>0,k>0)
+ nr <- 0
+ p <- 1
+┌ while n≠0 and k≠0 do
+│┌ if n%2=0 then
+││    nr <- nr+ n%10*p
+││    p <- p*10
+││ else
+││    k <-  k-1
+│└■
+│  n <- [n/10]
+└■
+ write nr 
+*)
 open System
-printf "Dati n="
-let n=int(System.Console.ReadLine())
-printf "Dati k="
-let k=int(System.Console.ReadLine())
-let rec calcul n k p nr=
+printf " n="
+let n=int(Console.ReadLine())
+printf " k="
+let k=int(Console.ReadLine())
+let rec whileLoop n k p nr=
     match n<>0 && k<>0 with
     | false -> nr
     | true -> match n%2=0 with
-              | true -> calcul (n/10) k (p*10) (nr+n%10*p)
-              | false -> calcul (n/10) (k-1) p nr 
+              | true -> whileLoop (n/10) k (p*10) (nr+n%10*p)
+              | false -> whileLoop (n/10) (k-1) p nr 
 
-let res = calcul n k 1 0
+let res = whileLoop n k 1 0
 printfn "%i" res 
