33c3ad9b2be7cf884335228732815877d373ffeb
Nelu Dascal <idascal@computervoice.ro>
2018-04-03 10:23:37 +0300

    Added comments to BAC var 9 sub I p2

--- var9subI_p2.fs
+++ var9subI_p2.fs
@@ -1,12 +1,25 @@
+(*
+ read n ( natural number)
+ s <- 10
+┌ while n>0 do
+│┌ if n%10<s then
+││    s <- n%10
+││ else
+││    s <- -1
+│└■
+│ n <- [n/10]
+└■
+  write s
+*)
 open System
-printf "Dati n="
-let n=int(System.Console.ReadLine())
-let rec calcul s n=
+printf " n="
+let n=int(Console.ReadLine())
+let rec whileLoop s n=
     match n with 
     |0 -> s
     | _ -> match n%10<s with
-           | true -> calcul (n%10) (n/10)
-           | false -> calcul -1 (n/10)
+           | true -> whileLoop (n%10) (n/10)
+           | false -> whileLoop -1 (n/10)
 
-let res=calcul 10 n
+let res=whileLoop 10 n
 printfn "%i" res
