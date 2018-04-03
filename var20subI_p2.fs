e2e49b6ae9faab1473820203251d60a0b95a104d
Nelu Dascal <idascal@computervoice.ro>
2018-04-03 10:38:54 +0300

    Added comments to BAC var 20 sub I p2

--- var20subI_p2.fs
+++ var20subI_p2.fs
@@ -1,18 +1,28 @@
+(*
+ read n ( natural number)
+ a <- n%10
+ m <- a
+┌ while n>9 do
+│   n <- [n/10]
+│   b <- n%10
+│┌ if a>b then
+││    m <- m*10+b
+││    a <- b
+│└■
+└■
+ write m
+*)
 open System
-printf "Dati n="
-let n=int(System.Console.ReadLine())
-let rec calcul a b m n=
-    printf "a=%i    " a
-    printf "b=%i    " b
-    printf "m=%i    " m
-    printfn "n=%i" n
+printf " n="
+let n=int(Console.ReadLine())
+let rec whileLoop a b m n=
     match n>0 with
     | false -> m 
     | true -> if a>b then
-                  calcul b (n/10%10) (m*10+b) (n/10)
+                  whileLoop b (n/10%10) (m*10+b) (n/10)
               else 
-                  calcul a (n/10%10) m (n/10)
+                  whileLoop a (n/10%10) m (n/10)
 
-let res=calcul (n%10) (n/10%10) (n%10) n
+let res=whileLoop (n%10) (n/10%10) (n%10) n
 printfn "%i" res
         
