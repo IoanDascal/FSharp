3812941d728a0dc7b66896d26dadaa23a7960c23
Nelu Dascal <idascal@computervoice.ro>
2018-04-03 10:31:40 +0300

    Added comments to BAC var 19 sub I p2

--- var19subI_p2.fs
+++ var19subI_p2.fs
@@ -1,15 +1,25 @@
+(*
+ read a,b ( natural numbers)
+ a <- [a/10]%10*10+a%10
+ b <- [b/10]%10*10+b%10
+┌ for i <- a,b do
+│┌ if [i/10]=i%10 then
+││     write i%10
+│└■
+└■
+*)
 open System
-printf "Dati a="
-let a=int(System.Console.ReadLine())
-printf "Dati b="
-let b=int(System.Console.ReadLine())
+printf " a="
+let a=int(Console.ReadLine())
+printf " b="
+let b=int(Console.ReadLine())
 let x=a/10%10*10+a%10
 let y=b/10%10*10+b%10
-let rec calcul x y=
+let rec forLoop x y=
     match x>y with
     | true -> printfn ""
     | false -> match x/10=x%10 with
-               | false -> calcul (x+1) y
+               | false -> forLoop (x+1) y
                | true -> printf "%i" (x%10)
-                         calcul (x+1) y
-let res=calcul x y
+                         forLoop (x+1) y
+let res=forLoop x y
