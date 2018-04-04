75210cefa8dd549c7bed0960dec05b513e466b1f
Nelu Dascal <idascal@computervoice.ro>
2018-04-04 10:52:55 +0300

    Added comments to BAC var 32 sub I p 2

--- var32subI_p2.fs
+++ var32subI_p2.fs
@@ -1,16 +1,28 @@
-printf "Dati a="
+(*
+ read a,b ( natural numbers)
+┌ if a>b then
+│    c <- b
+│    b <- a
+│    a <- c
+└■
+┌ while a<=b do
+│    write a
+│    a <- a*2
+└■
+ write a 
+*)
+printf " a="
 let a=int(System.Console.ReadLine())
-printf "Dati b="
+printf " b="
 let b=int(System.Console.ReadLine())
-let swap (a,b)=(b,a)
-let res=if a>b then swap (a,b)
+let ab=if a>b then (b,a)
             else   
                 (a,b)
-let rec afis x y=
+let rec whileLoop x y=
     match x<=y with
     | true -> printf "%i  " x
-              afis (2*x) y   
+              whileLoop (2*x) y   
     |false -> ()
 
-let rez=afis (fst res) (snd res)
+let res=whileLoop (fst ab) (snd ab)
 printfn ""
