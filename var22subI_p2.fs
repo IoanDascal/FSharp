c972c8eb833633715103141dde2c45ecbfa7276c
Nelu Dascal <idascal@computervoice.ro>
2018-04-04 08:00:51 +0300

    Added comments to BAC var 22 sub I p 2

--- var22subI_p2.fs
+++ var22subI_p2.fs
@@ -1,18 +1,25 @@
-open System
-printf "Dati n="
-let n=abs (int(System.Console.ReadLine()))
 (*
-let modul n=
-    match n<0 with
-    | true -> -n 
-    | false -> n
-let m=modul n
+ read n ( înteger number )
+┌ if n < 0 then
+│    n <- -n
+└■
+ d <- 1
+┌ for i <- 2,[n/2] do
+│┌ if i|n then
+││    d <- i
+│└■
+└■
+ write d 
 *)
-let rec calcul i div m=
+open System
+printf " n="
+let n=int32(Console.ReadLine())
+let m=if n<0 then -n else n
+let rec forLoop i d m=
     match i<=m/2 with
-    | false -> div
+    | false -> d
     | true -> match m%i with 
-              | 0 -> calcul (i+1) i m
-              | _ -> calcul (i+1) div m 
-let d=calcul 2 1 n
+              | 0 -> forLoop (i+1) i m
+              | _ -> forLoop (i+1) d m 
+let d=forLoop 2 1 m
 printfn "%i" d 
