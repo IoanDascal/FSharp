8d9706c7ade024488af84a6798ae6908f80e48cb
Nelu Dascal <idascal@computervoice.ro>
2018-04-04 07:43:25 +0300

    Added comments to BAC var 21 sub I p 2

--- var21subI_p2.fs
+++ var21subI_p2.fs
@@ -1,23 +1,40 @@
+(*
+ read a,b,n ( natural numbers)
+┌ if b=0 then
+│    write ”wrong”
+│ else
+│    write [a/b]
+│ ┌ if n>0 and a%b≠0 then
+│ │    write ”,”
+│ │    a <- a%b; i <- 0
+│ │ ┌ repeat
+│ │ │     write [(a*10)/b]
+│ │ │     a <- (a*10)%b
+│ │ │     i <- i+1
+│ │ └ until i=n or a=0
+│ └■
+└■
+*)
 open System
-printf "Dati a="
-let a=int(System.Console.ReadLine())
-printf "Dati b="
-let b=int(System.Console.ReadLine())
-printf "Dati n="
-let n=int(System.Console.ReadLine())
-let rec loop i a=
+printf " a="
+let a=int(Console.ReadLine())
+printf " b="
+let b=int(Console.ReadLine())
+printf " n="
+let n=int(Console.ReadLine())
+let rec repeatLoop i a=
+    printf "%i" (a*10/b)
     match i=n || a=0 with
     | true -> printf ".."
-    | false -> printf "%i" (a*10/b)
-               loop (i+1) (a*10%b)
-let calcul a b n=
+    | false -> repeatLoop (i+1) (a*10%b)
+let result a b n=
     match b with
-    | 0 -> printf "gresit"
+    | 0 -> printf "wrong"
     | _ -> printf "%i" (a/b)
            match n>0 && a%b<>0 with
            | false -> printfn ""
            | true -> printf ","
-                     loop 0 (a%b)
+                     repeatLoop 0 (a%b)
 
-let res=calcul a b n
+let res=result a b n
 printfn ""
