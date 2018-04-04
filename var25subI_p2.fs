a65e50413eb363b12e1c46e126c5c8efd03b54ae
Nelu Dascal <idascal@computervoice.ro>
2018-04-04 08:59:35 +0300

    Added comments to BAC var 25 sub I p 2

--- var25subI_p2.fs
+++ var25subI_p2.fs
@@ -1,16 +1,27 @@
+(*
+ read a,b,c  ( natural numbers >0)
+┌ if a>b then
+│    t <- a; a <- b; b <- t
+└■
+┌ while a≤b do
+│ ┌ if c|a then
+│ │    write a
+│ └■
+│   a <- a+1
+└■
+*)
 open System
-printf "Dati a="
-let a =int(System.Console.ReadLine())
-printf "Dati b="
-let b=int(System.Console.ReadLine())
-printf "Dati c="
-let c=int(System.Console.ReadLine())
-let swap (x,y)=(y,x)
-let compar=if a>b then swap (a,b) else (a,b)
-let rec afisare a b c=
+printf " a="
+let a =int(Console.ReadLine())
+printf " b="
+let b=int(Console.ReadLine())
+printf " c="
+let c=int(Console.ReadLine())
+let ab=if a>b then (b,a) else (a,b)
+let rec whileLoop a b c=
     match a<=b with
     | true -> if a%c=0 then printf "%i  " a
-              afisare (a+1) b c 
+              whileLoop (a+1) b c 
     | false -> ()
-let res=afisare (fst compar) (snd compar) c 
+let res=whileLoop (fst ab) (snd ab) c 
 printfn ""
