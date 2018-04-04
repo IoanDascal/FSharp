b7551286f74be33c77ce8ca358c2e804a2ae7ba7
Nelu Dascal <idascal@computervoice.ro>
2018-04-04 08:29:41 +0300

    Added comments to BAC var 23 sub I p 2

--- var23subI_p2.fs
+++ var23subI_p2.fs
@@ -1,15 +1,31 @@
+ (*
+  read a,b  ( natural numbers >0)
+  c <- 0
+┌ repeat
+│   i <- a%2
+│   j <- b%2
+│ ┌ if i+j=0 then
+│ │    c <- c+1
+│ └■
+│  a <- a*i+(1-i)*[a/2]
+│  b <- b*j+(1-j)*[b/2]
+└■ until i*j=1
+ write c
+*)
 open System
-printf "Dati a="
-let a=int(System.Console.ReadLine())
-printf "Dati b="
-let b=int(System.Console.ReadLine())
-let rec calcul a b c=
-    match ((a%2)*(b%2)) with
-    | 0 -> match (a%2+b%2) with
-           | 0 -> calcul (a*(a%2)+(1-a%2)*(a/2)) (b*(b%2)+(1-b%2)*(b/2)) (c+1)
-           | _ -> calcul (a*(a%2)+(1-a%2)*(a/2)) (b*(b%2)+(1-b%2)*(b/2)) c
-    | _ -> c
+printf " a="
+let a=int(Console.ReadLine())
+printf " b="
+let b=int(Console.ReadLine())
+let rec repeatLoop a b c=
+    let i=a%2
+    let j=b%2 
+    match i*j with
+    | 1 -> c
+    | _ -> match (i+j) with
+           | 0 -> repeatLoop (a*i+(1-i)*(a/2)) (b*(j)+(1-j)*(b/2)) (c+1)
+           | _ -> repeatLoop (a*(i)+(1-i)*(a/2)) (b*(j)+(1-j)*(b/2)) c
 
-let res=calcul a b 0
+let res=repeatLoop a b 0
 printfn "%i" res
 printfn ""
