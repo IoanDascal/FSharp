95de53f12308292acefc84f89beb948264c129de
Nelu Dascal <idascal@computervoice.ro>
2018-04-04 10:38:05 +0300

    Added comments to BAC var 30 sub I p 2

--- var30subI_p2.fs
+++ var30subI_p2.fs
@@ -1,9 +1,25 @@
-printf "Dati n="
+(*
+ read n ( natural number)
+ m <- 0
+ p <- 1
+┌ while n>0 do
+│    c <- n%10
+│ ┌ if c>0 then
+│ │    c <- c-1
+│ └■
+│   m <- m+c*p
+│   p <- p*10
+│   n <- [n/10]
+└■
+ write m 
+*)
+printf " n="
 let n=int(System.Console.ReadLine())
-let rec calcul n m p c=
+let rec whileLoop n m p c=
     match n>0 with
     | false -> m
     | true -> match c>0 with
-              | true -> calcul (n/10) (m+(c-1)*p) (p*10) (n/10%10)
-              | false -> calcul (n/10) (m+c*p) (p*10) (n/10%10)
-let res=calcul n 0 1 (n%10)
+              | true -> whileLoop (n/10) (m+(c-1)*p) (p*10) (n/10%10)
+              | false -> whileLoop (n/10) (m+c*p) (p*10) (n/10%10)
+let m=whileLoop n 0 1 (n%10)
+printfn "%i" m
