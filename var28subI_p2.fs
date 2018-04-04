5c41904fef2c6fc2799426731be5d223b6c2e008
Nelu Dascal <idascal@computervoice.ro>
2018-04-04 10:20:35 +0300

    Added comments to BAC var 28 sub I p 2

--- var28subI_p2.fs
+++ var28subI_p2.fs
@@ -1,13 +1,25 @@
-printf "Dati x="
+(*
+ read x  ( real number >0)
+ y <- [x]
+ x <- x-y
+┌ while x≠[x] do
+│    x <- x*10
+└■
+┌ if x=y then
+│    write 1
+│ else
+│    write 2
+└■
+*)
+printf " x="
 let x=double(System.Console.ReadLine())
 let y=floor x
 let z=round((x-y)*10000000000.0)/10000000000.0
-let rec calcul (z:double)=
-    printf "%0.15f " z
+let rec whileLoop (z:double)=
     match z<>(round z) with
     | false -> z
-    | true -> calcul (z*10.0)
+    | true -> whileLoop (z*10.0)
 
-let res=calcul z
+let res=whileLoop z
 if y=res then printfn "1"
     else printfn "2"
