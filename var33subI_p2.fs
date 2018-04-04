316080165ce73ac8dfeeac206117c9c324d0afc9
Nelu Dascal <idascal@computervoice.ro>
2018-04-04 10:58:33 +0300

    Added comments to BAC var 33 sub I p 2

--- var33subI_p2.fs
+++ var33subI_p2.fs
@@ -1,13 +1,26 @@
-printf "Dati a="
+(*
+ read a,n  ( natural numbers)
+ j <- 3
+┌ for i=1,n do
+│┌ if i%2=0 then
+││    a <- a-j
+││ else
+││    a <- a+j
+│└■
+│  j <- 7-j
+└■
+ write a
+*)
+printf " a="
 let a=int(System.Console.ReadLine())
-printf "Dati n="
+printf " n="
 let n=int(System.Console.ReadLine())
-let rec calcul a i j=
+let rec forLoop a i j=
     match i<=n with
     | false -> a
     | true -> match i%2=0 with
-              | true -> calcul (a-j) (i+1) (7-j)  
-              | false -> calcul (a+j) (i+1) (7-j)
+              | true -> forLoop (a-j) (i+1) (7-j)  
+              | false -> forLoop (a+j) (i+1) (7-j)
 
-let res= calcul a 1 3
+let res= forLoop a 1 3
 printfn "%i" res
