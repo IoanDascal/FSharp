15980d72a5a120743117da7e2c37c3ab78e3b443
Ioan Dascal <profuinfo2000@gmail.com>
2018-04-09 22:03:13 +0300

    Added comments BAC var 3 sub III p 2

--- var3subIII_p2.fs
+++ var3subIII_p2.fs
@@ -1,10 +1,28 @@
-let rec f x=
-    if x>0 then
-        match x%4 with
-        | 0 -> printf "x"
-               f (x-1)
-        | _ -> f (x/3)
-               printf "y"
+(*
+    Implement in F# next function:
+void f (int x) 
+{   
+    if(x>0) 
+        if(x%4==0) 
+        { 
+            cout<<’x’;
+            f(x-1); 
+        } 
+        else 
+        { 
+            f(x/3); 
+            cout<<’y’; 
+        } 
+} 
+*)
 
+let rec f x=
+    match x>0 with
+    | false -> ()
+    | true -> match x%4=0 with
+              | true -> printf "x"
+                        f (x-1)
+              | false -> f (x/3)
+                         printf "y"
 
 let res=f 26
