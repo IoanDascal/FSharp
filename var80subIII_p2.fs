6f14bcf6e898dc9966030a9572ec42fbd0ae6712
Nelu Dascal <idascal@computervoice.ro>
2018-04-13 09:20:10 +0300

    Added var 80 sub III p2

---
+++ var80subIII_p2.fs
@@ -0,0 +1,25 @@
+(*
+    Implement in F# next C++ function:
+ long sub(long n)
+ {
+     if (n!=0)
+         if(n%2=0) 
+             return n%10+sub(n/10);
+         else 
+             return sub(n/10);
+    else 
+        return 0;
+} 
+*)
+
+let rec sub n=
+    match n<>0 with
+    | false -> 0
+    | true -> match n%2=0 with
+              | true -> (n%10)+sub (n/10)
+              | false -> sub (n/10)
+
+let res=sub 132764
+printfn "res=%i" res
+let res1=sub 9
+printfn "res1=%i" res1
