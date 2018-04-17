6f97e0fdd9f7a3f35b18988869cfc9c2a0b512b6
Nelu Dascal <idascal@computervoice.ro>
2018-04-17 09:17:36 +0300

    Added Lucas function (same as Fibonacci)

---
+++ Lucas.fs
@@ -0,0 +1,12 @@
+(*
+    Definition of Lucas numbers using pattern matching.
+This is a tail recursion function.
+*)
+let rec Lucas x=
+    match x with
+    | x when x<=0 -> failwith "value must be greather than 0"
+    | 1 -> 1
+    | 2 -> 3
+    | x -> Lucas (x-1) + Lucas (x-2)
+
+printfn "Lucas 6=%i" (Lucas 6)
