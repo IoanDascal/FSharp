2d9b4aac51c7e329ecb2c87a6ea203ce924256df
Ioan Dascal <profuinfo2000@gmail.com>
2018-04-30 22:57:10 +0300

    Added comments to var 2 sub III pr 3

--- var2subIII_p3.fs
+++ var2subIII_p3.fs
@@ -1,20 +1,26 @@
-open System.IO
-open System.Net
+(*
+    File nr.txt contains integer numbers. 
+Write an application that read the numbers from file
+and prints only the natural numbers greather than zero
+in increasing order.
+*)
 
-let citire=
+open System.IO
+
+let readFile=
     use input=File.OpenText("nr.txt")
     let str=input.ReadToEnd()
     let str=str.Replace(System.Environment.NewLine," ")
     let res=str.Split[|' '|]
     res
 
-let sir=citire
-let numere=sir.[..sir.Length-1] |> Array.map System.Int32.Parse
-                                |> Array.filter (fun x -> x>0)
+let inputArray=readFile
+let numbers=inputArray.[..inputArray.Length-1] |> Array.map System.Int32.Parse
+                                              |> Array.filter (fun x -> x>0)
 
-let nr=Array.length numere
+let nr=Array.length numbers
 printfn "%A" nr
-if nr=0 then printfn "Nu exista"
+if nr=0 then printfn "There are no natural numbers greather than zero"
     else
-        let res=Array.sort numere
+        let res=Array.sort numbers
         printfn "%A" res
