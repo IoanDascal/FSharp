2dc3fd5f95fffbbfb6eee1c53a33de648c11e6ff
Nelu Dascal <idascal@computervoice.ro>
2018-05-11 08:48:27 +0300

    Added comments to var 4 sub II pr 2,5

--- var4subII_p2.fs
+++ var4subII_p2.fs
@@ -1,11 +1,15 @@
+(*
+     The parents array of a tree with n=8 vertices is :
+fathers=(6,5,5,2,0,3,3,3).  How many vertices are leafs.
+*)
 open System
-printf "Dati numarul de noduri n="
-let n=int32(System.Console.ReadLine())
-let vectorTati:int32 list=[for i in 1..n do 
-                               printf "v[%i]=" i
-                               yield int32(System.Console.ReadLine())]
-printfn "Vectorul de tati este : %A" vectorTati
-let frunze=List.filter (fun x -> not (List.contains x vectorTati)) [1..n]
-printfn "Frunzele sunt : %A" frunze
-printfn "Arborele are %i frunze" frunze.Length
-printfn "Radacina arborelui este %i" ((List.findIndex (fun elem -> elem=0) vectorTati)+1) 
+printf "Enter n="
+let n=int32(Console.ReadLine())
+let fathers=[for i in 1..n do
+              yield(printf "fathers[%i]=" i
+                    int(Console.ReadLine()))]
+printfn "The fathers array is : %A" fathers
+let leafs=List.filter (fun x -> not (List.contains x fathers)) [1..n]
+printfn "The leafs are : %A" leafs
+printfn "The tree has  %i leafs" leafs.Length
+printfn "The root of the tree is %i" ((List.findIndex (fun elem -> elem=0) fathers)+1) 
--- var4subII_p5.fs
+++ var4subII_p5.fs
@@ -1,5 +1,13 @@
+(*
+    Write a fuction to generate a matrix with n rows and
+n columns with elements equals to n-row. The elements from
+the antidiagonal must have their value equal to 0.
+*)
 open System
-printf "Dati un numar n="
-let n=int32(System.Console.ReadLine())
-let matrice=Array2D.init<int> n n (fun row col -> if row+col=n-1 then 0 else n-row)
-printfn "%A" matrice
+printf "Enter number of rows and columns n="
+let n=int32(Console.ReadLine())
+let matrix=Array2D.init<int> n n (fun row col -> if row+col=n-1 then 0 else n-row)
+for i in 0..n-1 do
+    for j in 0..n-1 do
+        printf "%i " matrix.[i,j]
+    printfn ""
