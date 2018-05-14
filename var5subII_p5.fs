c442561025e4886a78be63f16b8fd03ad6937228
Nelu Dascal <idascal@computervoice.ro>
2018-05-14 09:05:32 +0300

    Added comments to var 5 sub II pr 5

--- var5subII_p5.fs
+++ var5subII_p5.fs
@@ -1,7 +1,15 @@
+(*
+    Generate a matrix with n rows and m columns in wich elements are 
+equals to maximum between i and j; where i is the number of the current row
+and j is the number of the current column.
+*)
 open System
-printf "Dati n="
-let n=int32(System.Console.ReadLine())
-printf "Dati m="
-let m=int32(System.Console.ReadLine())
-let A=Array2D.init<int> n m (fun row col -> if row>col then row+1 else col+1)
-printfn "%A" A
+printf "Enter n="
+let n=int32(Console.ReadLine())
+printf "Enter m="
+let m=int32(Console.ReadLine())
+let matrix=Array2D.init<int> n m (fun row column -> if row>column then row+1 else column+1)
+for i in 0..n-1 do
+    for j in 0..m-1 do  
+        printf "%i " matrix.[i,j]
+    printfn ""
