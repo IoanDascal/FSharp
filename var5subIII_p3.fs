c34df84ad82fe34f2dd7110db9dd0c654285a038
Nelu Dascal <idascal@computervoice.ro>
2018-05-15 08:37:18 +0300

    Added comments to var 5 sub III pr 3,4

--- var5subIII_p3.fs
+++ var5subIII_p3.fs
@@ -1,12 +1,17 @@
+(*
+    Write a function that reads an integer number n and writes 
+in file nrvar5.txt the value of n and all it's prefixes.
+Example : for n=10305 file nrvar5.txt must contain :
+10305 1030 103 10 1
+*)
 open System.IO
-open System.Net
-printf "Dati n="
+printf "Enter n="
 let n=int32(System.Console.ReadLine())
-let rec scrie (acc:string) (nr:int)=
+let rec writeInFile (acc:string) (nr:int)=
     match nr with
     | 0 -> acc
-    | _ -> scrie (acc+" "+nr.ToString()) (nr/10)
+    | _ -> writeInFile (acc+" "+nr.ToString()) (nr/10)
 
-let rez=scrie "" n
+let rez=writeInFile "" n
 printfn "%s" rez
 File.WriteAllText("nrvar5.txt", rez)
--- var5subIII_p4.fs
+++ var5subIII_p4.fs
@@ -1,19 +1,26 @@
+(*
+    - Write a function f:int -> int that return the lowest prime
+divisor of an integer number a;
+    - Using function f write another function that reads n integer
+numbers and prints in increasing order only the prime numbers, 
+if there exists.
+*)
 open System
-printf "Dati n="
-let n=int32(System.Console.ReadLine())
-let rec divizor nr div=
+printf "Enter n="
+let n=int32(Console.ReadLine())
+let rec lowestPrimeDivisor nr div=
    match nr%div with
    | 0 -> div
-   | _ -> divizor nr (div+1)
+   | _ -> lowestPrimeDivisor nr (div+1)
 
-let test1=divizor n 2
-printfn "Divizorul este=%i" test1
-let prime=[|for i in 1..n do 
+let test1=lowestPrimeDivisor n 2
+printfn "The divisor is =%i" test1
+let primes=[|for i in 1..n do 
                printf "sir[%i]=" i
-               yield int32(System.Console.ReadLine())|] 
-           |> Array.filter (fun x -> x=(divizor x 2))
+               yield int32(Console.ReadLine())|] 
+           |> Array.filter (fun x -> x=(lowestPrimeDivisor x 2))
            |> Array.sort
 
-if prime.Length=0 then printfn "Nu exista"
+if primes.Length=0 then printfn "There are no prime numbers"
     else
-        printf "%A" prime
+        printfn "%A" primes
