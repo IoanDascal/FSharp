5df4404e5414543d3107cc3deb6acea3912f7d40
Ioan Dascal <profuinfo2000@gmail.com>
2018-05-15 21:53:39 +0300

    Added comments to var 6 sub II pr 5

--- var6subII_p5.fs
+++ var6subII_p5.fs
@@ -1,11 +1,15 @@
+(*
+    Given a text with many words that contains only small letters,
+write a function that transform first letter from every word to upper case.
+*)
 open System
-printf "Dati un text : "
-let text=System.Console.ReadLine()
-let textUP (inputString:string)=
+printf "Enter text : "
+let inputString=Console.ReadLine()
+let textToUpper (inputString:string)=
     String.mapi (fun i c -> match i with
                             | 0 -> Char.ToUpper(c)
                             | _ ->if Char.IsWhiteSpace(inputString.[i-1]) && Char.IsLetter(c) then 
                                       Char.ToUpper(c) 
                                       else  c) inputString
-let res=textUP text
+let res=textToUpper inputString
 printfn "%s" res
