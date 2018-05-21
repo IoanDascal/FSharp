faa77ea15be09f159977860042ae6f2adc9221a5
Nelu Dascal <idascal@computervoice.ro>
2018-05-21 09:09:37 +0300

    Added backtracking for permutations without repetitions

---
+++ Permutations.fs
@@ -0,0 +1,30 @@
+(*
+    Computes the permutations without repetitions with
+elements from a set of n elements.
+    
+*)
+
+open System
+printf "Enter number of elements from a set n="
+let n=int32(Console.ReadLine())
+let k=n
+let solution=Array.create 20 0
+let validatePartialSolution p=
+    if p=1 then true else not (Array.exists (fun elem -> elem=solution.[p]) solution.[1..p-1])
+let completeSolution p=
+    p=k
+
+let printSolution p=
+    for i in 1..p do
+        printf "%i" solution.[i]
+    printfn "" 
+let rec backtracking p=
+    for i in 1..n do
+        solution.[p] <- i
+        if (validatePartialSolution p) then
+            if (completeSolution p) then
+                printSolution p
+                else
+                    backtracking (p+1)
+
+let rez=backtracking 1
