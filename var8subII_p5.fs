65334bf9833f91db572ba04f9a2fc79b83759d1e
Nelu Dascal <idascal@computervoice.ro>
2018-06-06 10:57:39 +0300

    Added comments to var 8 sub II pr 5

--- var8subII_p5.fs
+++ var8subII_p5.fs
@@ -1,10 +1,14 @@
+(*
+    Generate a matrix in which elements are squares
+of odd numbers in increasing order.
+*)
 open System
-printf "Dati n="
-let n=int(System.Console.ReadLine())
-printf "Dati p="
-let p=int(System.Console.ReadLine())
-let mutable impar=1
-let lista=[for i in 1..n do 
-               yield [for j in 1..p do yield (pown impar 2);impar <- impar+2]]
-
-printfn "%A" lista
+printf "Enter n="
+let n=int(Console.ReadLine())
+printf "Enter p="
+let p=int(Console.ReadLine())
+let mutable odd=1
+let matrix=[for i in 1..n do 
+               yield [for j in 1..p do yield (pown odd 2);odd <- odd+2]]
+for el in matrix do   
+    printfn "%A" el
