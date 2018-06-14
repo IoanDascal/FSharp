5a64af373218897097ec63e41f26fcc8b718de0c
Nelu Dascal <idascal@computervoice.ro>
2018-06-14 09:55:27 +0300

    Added comments to var 14 sub II pr 2

--- var14subII_p2.fs
+++ var14subII_p2.fs
@@ -1,13 +1,17 @@
+(*
+    Given a linked list, write a function to calculate the 
+sum of elements divisible with 5.
+*)
 open System.Collections.Generic
 let linkedList=new LinkedList<int>()
-printf "Dati numarul de noduri din lista n="
+printf "Enter the number of nodes from list n="
 let n=int(System.Console.ReadLine())
 for i in 1..n do
-    printf "Dati lista[%i]=" i
+    printf "Enter list[%i]=" i
     let list=linkedList.AddFirst(int(System.Console.ReadLine()))
     list |> ignore
 
-let sumaDiv5= Seq.fold (fun acc x -> match x%5 with
+let sumDiv5= Seq.fold (fun acc x -> match x%5 with
                                      | 0 -> acc+x
                                      | _ -> acc) 0 linkedList
-printfn "Suma nr .div cu 5 =%i" sumaDiv5
+printfn "The sum of elements divisible with 5 is =%i" sumDiv5
