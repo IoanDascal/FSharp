5cdeb8af8f041a5b65a49db01ac70ea340c4424b
Nelu Dascal <idascal@computervoice.ro>
2018-06-26 10:15:48 +0300

    Added comments to var 22 sub III pr 3,4

--- var22subIII_p3.fs
+++ var22subIII_p3.fs
@@ -1,28 +1,47 @@
+(*
+    a) Write a function that return how many 0 digits are at the end
+of the n! (factorial).
+    b) Using the function from a) write another function that return the 
+lowest number n for which n! contains minimum k digits of zero at the end.
+*)
 open System
-printf "Dati n="
-let n=int(System.Console.ReadLine())
+///  a)
+printf "Enter n="
+
+let n=int(Console.ReadLine())
 let nz n=
     let rec factorial n acc=
         if n<=1I then acc 
             else
                 factorial (n-1I) (n*acc)
     let fact=factorial n 1I
-    let rec nrzero (fact:bigint) zero=
+    let rec nrZero (fact:bigint) zero=
         match fact%10I<>0I with
         | true -> zero
-        | _ -> nrzero (fact/10I) (zero+1)
-    nrzero fact 0
+        | _ -> nrZero (fact/10I) (zero+1)
+    nrZero fact 0
 let res=nz (bigint n)
-printfn "%i" res
-printf "Dati k="
-let k=int(System.Console.ReadLine())
+printfn "The number of zero digits at the end of n! is=%i" res
+///  b)
+printf "Enter k="
+let k=int(Console.ReadLine())
 
-let rec nrMinim k (nrMic:bigint)=
-    let k1=nz nrMic
-    match k1=k with
-    | true -> nrMic
-    | false -> nrMinim k (nrMic+1I) 
+let lowestNumber k=
+    let rec aproximation nr=
+        let k1=nz nr
+        match k1<k with
+        | true -> aproximation (2I*nr)
+        | false -> nr
+    let aprox=aproximation 1I
+    let nr=aprox/2I;
+    let rec lowest nr=
+        let k1=nz nr
+        match k1=k with
+        | true -> nr
+        | false -> lowest (nr+1I)
+    lowest nr
+    
 
-let b=nrMinim k 1I
-printfn "Numarul este: %O" b
+let b=lowestNumber k
+printfn "The number n with minimum %i digits of zero at the end of n! is : %O" k b
 printfn ""
--- var22subIII_p4.fs
+++ var22subIII_p4.fs
@@ -1,20 +1,25 @@
+(*
+    File nrVar224.txt contains on the first line a value for the number nr, 
+and on each of the next nr lines a pair a,b of integer numbers. Write a function that
+return for each pair a,b the maximum number which is a power of two between a and b.
+*)
 open System.IO
-let input=File.OpenText("../nrVar224.txt")
-let citireLinie (streamReader:StreamReader)=
-    let linie=streamReader.ReadLine()
-    let linie=linie.Replace(System.Environment.NewLine," ")
-    let linie=linie.Split([|' '|])
-    linie
+let input=File.OpenText("C:/Users/Nelu/Desktop/fsvsc/FSharp/nrVar224.txt")
+let readLine (streamReader:StreamReader)=
+    let line=streamReader.ReadLine()
+    let line=line.Replace(System.Environment.NewLine," ")
+    let line=line.Split([|' '|])
+    line
 
-let rec detPutere2 a b putere=
-    if putere<=b then detPutere2 a b (putere*2)
-        else if putere/2>=a then (putere/2)
+let rec thePowerOf2 a b power=
+    if power<=b then thePowerOf2 a b (power*2)
+        else if power/2>=a then (power/2)
             else 0
-let nr=((citireLinie input) |> Array.map int).[0]
+let nr=((readLine input) |> Array.map int).[0]
 for i in 1..nr do
-    let ab=(citireLinie input) |> Array.map int
+    let ab=(readLine input) |> Array.map int
     let a=ab.[0]
     let b=ab.[1]
-    let putere2=detPutere2 a b 1
-    printf "%i " putere2
+    let powerOf2=thePowerOf2 a b 1
+    printfn "The biggest number, between %i and %i, that is a power of two is=%i " a b powerOf2
 printfn ""
