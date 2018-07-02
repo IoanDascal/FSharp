53b3152a79492d5976db1d3c3713f0b4f4a3d115
Nelu Dascal <idascal@computervoice.ro>
2018-07-02 10:00:00 +0300

    Added comments to var 24 sub III pr 3,4

--- var24subIII_p3.fs
+++ var24subIII_p3.fs
@@ -1,18 +1,25 @@
+(*
+    a. Write a function to calculate the maximum, minimum and
+the average of an array of integer numbers.
+    b. Write a function to calculate the average of an array of 
+numbers without the minimum and maximum value, using the function 
+from a.
+*)
 open System
-printf "Dati n="
-let n=int(System.Console.ReadLine())
+printf "Enter n="
+let n=int(Console.ReadLine())
 let x=[for i in 1..n do
            yield (printf "x[%i]=" i
-                  int(System.Console.ReadLine()))]
+                  int(Console.ReadLine()))]
 printfn "%A" x
 let pRes=
     let p (x:int list)=
-        let suma=List.sum x
-        let maxi=List.max x
-        let mini=List.min x
-        ((maxi,mini),suma)
+        let sum=List.sum x
+        let maximum=List.max x
+        let minimum=List.min x
+        ((maximum,minimum),sum)
     p x
 printfn "%A" pRes 
-let media=floor(float((snd pRes)-(fst (fst pRes))-(snd (fst pRes)))/float(x.Length-2)*1000.0)
-printfn "%.3f" (media/1000.0)
+let average=floor(float((snd pRes)-(fst (fst pRes))-(snd (fst pRes)))/float(x.Length-2)*1000.0)
+printfn "%.3f" (average/1000.0)
 
--- var24subIII_p4.fs
+++ var24subIII_p4.fs
@@ -1,12 +1,23 @@
+(*
+    File nrVar244.txt contains on the first line a value for an
+integer number n, on the next n lines it contains integer numbers
+in increasing order, and on the last line values for integer 
+numbers a and b.
+    Write a function to find the lowest number from the closed
+interval [a..b]. If there is no number, the function should print "NO"
+*)
 open System.IO
-let input=File.OpenText("../nrVar244.txt")
-let citireLinie (istr:StreamReader)=
-    let sir=istr.ReadLine()
-    let sir=sir.Split([|' '|])
-    sir
-let n=(Array.map int (citireLinie input)).[0]
+let input=File.OpenText("C:/Users/Nelu/Desktop/fsvsc/FSharp/nrVar244.txt")
+let readLine (istr:StreamReader)=
+    let inputString=istr.ReadLine()
+    let inputArray=inputString.Split([|' '|])
+    inputArray
+let n=(Array.map int (readLine input)).[0]
 let vector=[|for i in 1..n do
-                 yield((Array.map int (citireLinie input)).[0])|]
-let ab=Array.map int (citireLinie input)
-let primul=Array.find (fun x -> x>=ab.[0]) vector
-printfn "%i" primul
+                 yield((Array.map int (readLine input)).[0])|]
+let ab=Array.map int (readLine input)
+let first=Array.tryFind (fun x -> x>=ab.[0] && x<=ab.[1]) vector
+if first.IsSome then
+    printfn "%i" first.Value
+    else   
+       printfn "NO"
