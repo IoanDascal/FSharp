ba5f4c044b2d9b3ef49073103660d922cda42231
Nelu Dascal <idascal@computervoice.ro>
2018-07-16 09:44:33 +0300

    Added comments to var 27 sub II pr 3,4

--- var27subIII_p3.fs
+++ var27subIII_p3.fs
@@ -1,13 +1,19 @@
+(*
+    Write a function nreal : int -> int -> float , which 
+takes two integers x and y from closed interval [1..1000], and 
+returns a real number so that the whole number part is equal to x
+and the fractional part is equal to y.
+*)
 let nreal x y=
-    let zecimal y=
+    let fractional y=
         if y>=100 then float(y)/1000.0
         else if y>=10 && y<100 then float(y)/100.0
              else float(y)/10.0
-    let res=float(x)+(zecimal y)
+    let res=float(x)+(fractional y)
     res
-let nrNou=nreal 12 543
-printfn "%0.3f" nrNou
-let nrNou1=nreal 12 54
-printfn "%0.2f" nrNou1
-let nrNou2=nreal 12 5
-printfn "%0.1f" nrNou2
+let newNumber=nreal 12 543
+printfn "%0.3f" newNumber
+let newNumber1=nreal 12 54
+printfn "%0.2f" newNumber1
+let newNumber2=nreal 12 5
+printfn "%0.1f" newNumber2
--- var27subIII_p4.fs
+++ var27subIII_p4.fs
@@ -1,13 +1,21 @@
+(*
+    File nrVar274.txt contains on the first line a value for an integer 
+number n and on the next line n real numbers in increasing order.
+Write a program to calculate the minimum interger numbers that lies
+between endpoints of all closed intervals that can be obtained with 
+numbers from the second line of the file.
+*)
 open System.IO
-let input=File.OpenText("../nrVar274.txt")
-let citireLinie (input:StreamReader)=
-    let sir=input.ReadLine()
-    let sir=sir.Replace(System.Environment.NewLine," ")
-    let sir=sir.Split([|' '|])
-    sir
-let n=(citireLinie input).[0] |>int 
-let numere=(citireLinie input) |>Array.map float
-let diferente=[for i in 1..n-1 do
-                   yield(int(floor(numere.[i])-floor(numere.[i-1])))]
-let maxim=List.min diferente
-printfn "maxim=%i" maxim
+let input=File.OpenText("C:/Users/Nelu/Desktop/fsvsc/FSharp/nrVar274.txt")
+let readLine (input:StreamReader)=
+    let inputString=input.ReadLine()
+    let inputString=inputString.Replace(System.Environment.NewLine," ")
+    let inputArray=inputString.Split([|' '|])
+    inputArray
+let n=(readLine input).[0] |>int 
+let numbers=(readLine input) |>Array.map float
+let differences=[for i in 0..n-2 do
+                    for j in i+1..n-1 do
+                        yield(int(floor(numbers.[j])-floor(numbers.[i])))]
+let minimum=List.min differences
+printfn "Minimum =%i" minimum
