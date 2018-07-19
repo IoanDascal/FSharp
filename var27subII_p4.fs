ff4629fb7e22c4582bbde0abda25e6a21363588b
Nelu Dascal <idascal@computervoice.ro>
2018-07-19 14:40:07 +0300

    Added comments to var 27 sub II pr 4

--- var27subII_p4.fs
+++ var27subII_p4.fs
@@ -1,7 +1,8 @@
 (*
 
 *)
-//(*
+
+(*
 type Node=
     {
         mutable nr:int;
@@ -19,17 +20,7 @@ let rec addLast x l=
     match !l with
     | None -> l:=Some (singleton x) 
     | Some node -> addLast x node.next
-let newNode x (l:List)=
-    {
-        nr=x;
-        next=l
-    }
-let addFirst x l=
-    if l=empty then  l:=Some {nr=x;next=ref None}
-        else let node={nr=x;next=ref None} 
-             node.next:=!l
-             l:=Some node
-                                     
+                               
 let  first (list:List)=
     match !list with
     | None -> None
@@ -41,7 +32,7 @@ let nextNode (node:Node option)=
 let showNode (node:Node option)=
     if node.IsSome then printf "%i-" node.Value.nr
 
-let example1=
+let example=
     let l=empty
     addLast 1 l
     addLast 2 l
@@ -49,7 +40,7 @@ let example1=
     addLast 4 l
     addLast 5 l
     l
-let fn=first example1
+let fn=first example
 showNode fn
 let next=nextNode fn
 let fn1=first next
@@ -60,49 +51,62 @@ let rec showList (l:List)=
                       showList (nextNode fn)
                  else printfn "End of list"
 printf "Ex1 : "
-showList example1
+showList example
 printfn ""
+*)
 
-let example2=
-    let l1=empty
-    addFirst 1 l1
-    addFirst 2 l1
-    addFirst 3 l1
-    addFirst 4 l1
-    addFirst 5 l1
-    l1
-let fnn=first example2
-showNode fnn
-let nextn=nextNode fnn
-let fnn1=first nextn
-showNode fnn1
-printf "Ex 2 : "
-showList example2
-printfn ""
-//*)
-(*
 type LinkedList<'a>=
     | Empty  
     | LinkedList of 'a*LinkedList<'a>
 
+type Node=
+    {
+        mutable nr:int
+    }
+
 let rec addLast newValue=function
-    | Empty -> LinkedList (newValue,Empty)
-    | LinkedList (nr,linkedList) -> let linkedList=addLast newValue linkedList 
-                                    LinkedList (nr,linkedList)
-let insertValuesInLinkedList (values:'a array)=
-    let linkedList=values |> Array.fold (fun newList value -> addLast value newList) Empty
+    | Empty -> LinkedList ({nr=newValue},Empty)
+    | LinkedList (node,linkedList) -> let linkedList=addLast newValue linkedList 
+                                      LinkedList (node,linkedList)
+let insertValuesAtEndOfLinkedList (values:int array)=
+    let linkedList:LinkedList<Node>=values |> Array.fold (fun newList value -> addLast value newList) Empty
     linkedList
 let values=[|1..5|]
-let list=insertValuesInLinkedList values
-printfn "%A" list
+let list=insertValuesAtEndOfLinkedList values
+let rec showList (l:LinkedList<Node>)=
+    match l with
+    | Empty -> printfn "End of list"
+    | LinkedList (node,linkedList) -> printf "%i-" node.nr  
+                                      showList linkedList
+                     
 let addFirst newValue=function
-    | Empty -> LinkedList (newValue,Empty)
-    | LinkedList (nr,linkedList) -> let linkedList=LinkedList (nr,linkedList)
-                                    LinkedList (newValue,linkedList)
-let insertValuesInLinkedList1 (values:'a array)=
-    let linkedList=values |> Array.fold (fun newList value -> addFirst value newList) Empty
+    | Empty -> LinkedList ({nr=newValue},Empty)
+    | LinkedList (node,linkedList) -> let linkedList=LinkedList (node,linkedList)
+                                      LinkedList ({nr=newValue},linkedList)
+let insertValuesAtFrontOfLinkedList (values:int array)=
+    let linkedList:LinkedList<Node>=values |> Array.fold (fun newList value -> addFirst value newList) Empty
     linkedList
 
-let list1=insertValuesInLinkedList1 values
-printfn "%A" list1
-*)
+let list1=insertValuesAtFrontOfLinkedList values
+
+showList list1
+
+let first l=
+    match l with
+    | Empty -> {nr=1}
+    | LinkedList (node,_) -> node
+let nextNode (l:LinkedList<Node>)=
+    match l with
+    | Empty -> Empty
+    | LinkedList (_,linkedList) -> linkedList
+let modifyList (l:LinkedList<Node>)=
+    let prim=first l
+    let next=nextNode l
+    let rec newLst (p:Node) nx=
+        match nx with
+        | Empty -> ()
+        | LinkedList (node,linkedList) -> node.nr <- p.nr*node.nr
+                                          newLst node linkedList
+    newLst prim next
+modifyList list1
+showList list1
