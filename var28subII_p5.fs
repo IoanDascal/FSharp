50c23af5a5a93d4be5671de799010b8008e852d3
Nelu Dascal <idascal@computervoice.ro>
2018-07-20 11:53:15 +0300

    Added comments to var 28 sub II pr 5

--- var28subII_p5.fs
+++ var28subII_p5.fs
@@ -1,10 +1,14 @@
-let s1="informatica"
-let ch='a'
-let wovvelOut inputString ch=
-    let elimina ch=
+(*
+    Given an input string write a program that eliminates only one 
+vowel at a time and print the new string, if it exists.
+*)
+let s="instantaneously"
+
+let vowelOut inputString ch=
+    let removeVowel ch=
         String.collect (fun c -> if c=ch then sprintf ""
                                  else sprintf "%c" c) inputString
-    let sir=elimina ch
-    if sir.Length<> s1.Length then printfn "%s" sir
-let wovels="aeiou"
-let res=String.iter (fun x -> wovvelOut s1 x) wovels
+    let str=removeVowel ch
+    if str.Length<> s.Length then printfn "%s" str
+let vowels="aeiou"
+let res=String.iter (fun x -> vowelOut s x) vowels
