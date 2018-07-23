17d0673a6173dd367e063a1201885eb5eb6e11b7
Nelu Dascal <idascal@computervoice.ro>
2018-07-23 08:43:15 +0300

    Added comments to var 28 sub II pr 3,4

--- var28subIII_p3.fs
+++ var28subIII_p3.fs
@@ -1,12 +1,18 @@
+(*
+    Given an array of n real numbers write a program to calculate
+how many numbers are equals to the average between other n-1 numbers.
+*)
+
+open System
 printf "Dati n="
-let n=int(System.Console.ReadLine())
+let n=int(Console.ReadLine())
 let vector=[|for i in 1..n do
                  yield(printf "v[%i]=" i
-                       float(System.Console.ReadLine()))|] 
-let suma=Array.sum vector
-let medii=[|for i in 0..n-1 do yield((suma-vector.[i])/float(n-1))|]
+                       float(Console.ReadLine()))|] 
+let sum=Array.sum vector
+let averages=[|for i in 0..n-1 do yield((sum-vector.[i])/float(n-1))|]
 
-let res=Array.map2 ( fun x y -> if x=y then x else 0.0) vector medii
-let egale=Array.countBy (fun x -> if x<>0.0 then x else 0.0) res
-if (fst egale.[0])<>0.0 then printfn "Sunt egale : %i medii"  (snd egale.[0])
-    else printfn "Sunt egale : %i medii" (snd egale.[1])
+let res=Array.map2 ( fun x y -> if x=y then x else 0.0) vector averages
+let equals=Array.countBy (fun x -> if x<>0.0 then x else 0.0) res
+if (fst equals.[0])<>0.0 then printfn "Sunt egale : %i medii"  (snd equals.[0])
+    else printfn "Sunt egale : %i medii" (snd equals.[1])
--- var28subIII_p4.fs
+++ var28subIII_p4.fs
@@ -1,25 +1,32 @@
+(*
+    a. Write a function lowestDivisor : int -> int that returns
+the lowest prime divisor af a natural number.
+    b. File nrVar284.txt contains on the first line a value for n,
+and on the second line an array of n natural numbers. Using function 
+lowestDivisor : int -> int write a function to calculate the biggest 
+2-almost prime number from the array. If the array doesn't contain a
+2-almost prime numbers, the program will print the message:
+"There isn't a 2-almost prime number"
+*)
 open System.IO
-let input=File.OpenText("../nrVar284.txt")
-let citireLinie (inputReader:StreamReader)=
-    let sir=inputReader.ReadLine()
-    let sir=sir.Replace(System.Environment.NewLine," ")
-    let sir=sir.Split([|' '|])
-    sir
-//printf "Dati a="
-//let a=int(System.Console.ReadLine())
-let primul x=
-    let rec loop i=
-        match x%i=0 with
+let input=File.OpenText("C:/Users/Nelu/Desktop/fsvsc/FSharp/nrVar284.txt")
+let readLine (inputReader:StreamReader)=
+    let inputString=inputReader.ReadLine()
+    let inputString=inputString.Replace(System.Environment.NewLine," ")
+    let inputArray=inputString.Split([|' '|])
+    inputArray
+let lowestDivisor (x:uint32)=
+    let rec loop (i:uint32)=
+        match x%i=0u with
         | true -> i
-        | false -> loop (i+1)
-    loop 2
-let n=(citireLinie input).[0] |> int
-let numere=Array.map int (citireLinie input)
-let aproapePrim=[for i in 0..n-1 do
-                     yield(let div=primul numere.[i]
-                           let div1=primul (numere.[i]/div)
-                           if numere.[i]=div*div1 then numere.[i]
-                               else 0)]
-let maxim=List.max aproapePrim
-if maxim>0 then printfn "Cel mai mare numar aproape prim este =%i" maxim
-    else printfn "Nu"
+        | false -> loop (i+1u)
+    loop 2u
+let n=(readLine input).[0] |> uint32
+let numbers=Array.map uint32 (readLine input)
+let almostPrimes=Array.choose (fun x -> let div=lowestDivisor x
+                                        let div1=lowestDivisor (x/div)
+                                        if x=div*div1 then Some x
+                                            else None) numbers
+let maximum=Array.max almostPrimes
+if maximum>0u then printfn "The biggest number 2-almost prime is =%i" maximum
+    else printfn "There isn't a 2-almost prime number."
