63eec85f0d68e8b2b007694e60dd57875a0257c9
Nelu Dascal <idascal@computervoice.ro>
2018-07-27 10:09:54 +0300

    Added comments to var 29 sub II pr 3,5

--- var29subII_p3.fs
+++ var29subII_p3.fs
@@ -1,3 +1,12 @@
+(*
+    Implement in F# next C++ program sequence :
+    char s[]=”arac”;
+    t=s[1]; s[1]=s[3];
+    s[3]=’t’;
+    cout<<s;
+*)
 let s="arac"
-let res=String.mapi (fun i c -> if i=3 then 't' else c) s
+let res=String.mapi (fun i c -> if i=1 then s.[3] 
+                                    else if i=3 then 't' 
+                                        else c) s
 printfn "%s" res
--- var29subII_p5.fs
+++ var29subII_p5.fs
@@ -1,22 +1,38 @@
-printf "Dati numarul de linii si coloane n="
-let n = int(System.Console.ReadLine())
-let matrice=[|for i in 0..n-1 do 
-                 yield([|for j in 0..n-1 do
-                            yield(printf "mat[%i,%i]=" i j
-                                  int(System.Console.ReadLine()))|])|]
-for lista in matrice do
-    printfn "%A" lista
-let rec produsPivoti (coloana:int) (pivot:int)=
-    match coloana<n with
-    | false -> pivot
-    | true -> let raport=matrice.[0].[coloana]/matrice.[0].[0]
-              let rec verific i=
-                  if (i=n ||  matrice.[i].[coloana]/matrice.[i].[0]<>raport) then i
-                      else verific (i+1)
-              printfn "%i" (verific 1)
-              if (verific 1)=n then produsPivoti (coloana+1) (pivot*raport)
-                  else produsPivoti (coloana+1) pivot
+(*
+    Given a matrix with n rows and n columns, an integer number x is 
+called multiple if after multiplying of x with each element from the first 
+column it can be obtained all elements from another column. Compute the 
+product of all multiples from a matrix. 
+Example: For matrix  2  7  4  8  4
+                     1  1  2  4  2
+                     3 12  6 12  3
+                     1 22  2  4  2
+                     5 10 10 20  8
+the multiples are: 2 for columns 1 and 3
+                   4 for columns 1 and 4
+the product is : 2*4=8
+*)
 
-let res=produsPivoti 1 1
+open System
+printf "Enter the number of rows and columns n="
+let n = int(Console.ReadLine())
+let matrix=[|for i in 0..n-1 do 
+                 yield([|for j in 0..n-1 do
+                            yield(printf "matrix[%i,%i]=" i j
+                                  int(Console.ReadLine()))|])|]
+for row in matrix do
+    printfn "%A" row
+let rec multiplesProduct (column:int) (multiple:int)=
+    match column<n with
+    | false -> multiple
+    | true -> let quotient=matrix.[0].[column]/matrix.[0].[0]
+              let rec verify i=
+                  if (i=n ||  matrix.[i].[column]/matrix.[i].[0]<>quotient) then i
+                      else verify (i+1)
+              printfn "%i" (verify 1)
+              if (verify 1)=n then multiplesProduct (column+1) (multiple*quotient)
+                  else multiplesProduct (column+1) multiple
+
+let res=multiplesProduct 1 1
 printfn "%i" res
 
