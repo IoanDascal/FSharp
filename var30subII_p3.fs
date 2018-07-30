455e1a47f2bf5d15b8215be7275d3f381669a682
Nelu Dascal <idascal@computervoice.ro>
2018-07-30 09:22:19 +0300

    Added comments to var 30 sub II pr 3,5

--- var30subII_p3.fs
+++ var30subII_p3.fs
@@ -1,23 +1,28 @@
-printf "Dati numarul de noduri n="
-let n=int(System.Console.ReadLine())
+(*
+    Given a tree with n nodes, write a program to calculate
+the number of leafs from the adjacency matrix reprezentation.
+*)
 
-let matAdiacenta=Array2D.zeroCreate<int> (n+1) (n+1)
-printf "Dati numarul de muchii m="
-let m=int(System.Console.ReadLine())
-printf "Dati nodul radacina r="
-let r=int(System.Console.ReadLine())
+open System
+printf "Enter the number of nodes n="
+let n=int(Console.ReadLine())
+
+let adjacencyMatrix=Array2D.zeroCreate<int> (n+1) (n+1) 
+let m=n-1
+printf "Enter the root node r="
+let r=int(Console.ReadLine())
 for i in 1..m do
-    printf "Dati x="
-    let x=int(System.Console.ReadLine())
-    printf "Dati y="
-    let y=int(System.Console.ReadLine())
-    matAdiacenta.[x,y] <- 1
-    matAdiacenta.[y,x] <- 1
+    printf "Enter x="
+    let x=int(Console.ReadLine())
+    printf "Enter y="
+    let y=int(Console.ReadLine())
+    adjacencyMatrix.[x,y] <- 1
+    adjacencyMatrix.[y,x] <- 1
 
-let grade=[for i in 1..n do
-               yield(Array.sum matAdiacenta.[i,1..n])]
-let frunze=List.countBy (fun x -> x=1) grade
-let nrFrunze=if (fst frunze.[0]) then (snd frunze.[0])
-                 else (snd frunze.[1])
-if grade.[r-1]=1 then printfn "Numarul de frunze este : %i" (nrFrunze-1)
-    else printfn "Numarul de frunze este : %i" nrFrunze
+let degrees=[for i in 1..n do
+               yield(Array.sum adjacencyMatrix.[i,1..n])]
+let leafs=List.countBy (fun x -> x=1) degrees
+let nrOfLeafs=if (fst leafs.[0]) then (snd leafs.[0])
+                 else (snd leafs.[1])
+if degrees.[r-1]=1 then printfn "The number of leafs is : %i" (nrOfLeafs-1)
+    else printfn "The number of leafs is : %i" nrOfLeafs
--- var30subII_p5.fs
+++ var30subII_p5.fs
@@ -1,12 +1,15 @@
-printf "Dati numarul de linii si cloane n="
+(*
+    Write a program to generate a matrix so that the elements 
+from the first row and and from the first column are equal to 1
+and for other elements use the relation : A[i,j]=A[i,j-1]+A[i-1,j].
+*)
+printf "Enter the number of rows and columns n="
 let n=int(System.Console.ReadLine())
-let matrice=Array2D.create<int> n n 1
-printfn "%A" matrice
-printfn ""
+let matrix=Array2D.create<int> n n 1
 for i in 1..n-1 do
     for j in 1..n-1 do
-        matrice.[i,j] <- matrice.[i,j-1]+matrice.[i-1,j]
+        matrix.[i,j] <- matrix.[i,j-1]+matrix.[i-1,j]
 for i in 0..n-1 do
     for j in 0..n-1 do
-        printf "%i  " matrice.[i,j]
+        printf "%4i  " matrix.[i,j]
     printfn ""
