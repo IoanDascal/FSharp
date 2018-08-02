403a8ccdeb47a45a65a5a9da9d5441ef7cafb6a3
Nelu Dascal <idascal@computervoice.ro>
2018-08-02 15:19:06 +0300

    Added comments to var 31 sub II pr 2

--- var31subII_p2.fs
+++ var31subII_p2.fs
@@ -1,15 +1,25 @@
-printf "Dati numarul de noduri n="
-let n=int(System.Console.ReadLine())
-let tati=[for i in 1.. n do 
-              yield(printf "tati[%i]=" i
-                    int(System.Console.ReadLine()))]
-let rec calcul (ascendenti:int list)=
-    printfn "tata=%i" tati.[ascendenti.[0]]
-    printfn "asc=%A" ascendenti
-    System.Console.ReadKey() |> ignore
-    match tati.[ascendenti.[0]-1] with
-    | 0 -> ascendenti
-    |_ -> calcul (tati.[ascendenti.[0]-1]::ascendenti)
-let rez=calcul [8]
-printfn "%A" rez
+(*
+    Given the parents array representation of a tree, write a program 
+to make a list of all ancestors of a node x.
+*)
+open System
+printf "Enter the number of vertices n="
+let n=int(Console.ReadLine())
+let parents=[for i in 1.. n do 
+              yield(printf "parents[%i]=" i
+                    int(Console.ReadLine()))]
+printf "Enter x="
+let x=int(Console.ReadLine())
+let getAllAncestors (node:int)=
+    let rec getAncestors (ancestors:int list) node=
+        printfn "ancestors=%A" ancestors
+        printfn "Press any key : "
+        Console.ReadKey() |> ignore
+        match parents.[node-1] with
+        | 0 -> ancestors
+        | _ -> getAncestors (parents.[node-1]::ancestors) (parents.[node-1])
+    let ancestors=getAncestors [] node
+    ancestors
+let ancestors=getAllAncestors x
+printfn "The ancestors of node %i are : %A" x ancestors 
           
