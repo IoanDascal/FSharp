46fd4af7f82205646fa69b4265c9a469e2748071
Nelu Dascal <idascal@computervoice.ro>
2018-08-02 09:35:36 +0300

    Added comments to var 31 sub II pr 1

--- var31subII_p1.fs
+++ var31subII_p1.fs
@@ -1,38 +1,50 @@
-printf "Dati numarul de noduri n="
-let n=int(System.Console.ReadLine())
-printf "Dati numarul de muchii m="
-let m=int(System.Console.ReadLine())
-let matriceAdiacenta=Array2D.zeroCreate<int> (n+1) (n+1)
+(*
+    Given an undirected graph with n vertices and m edges,
+write a program to verify if a list of vertices is a Hamiltonian
+path using the adjacency matrix.
+Example: For n=7, m=10 and edges [1,3],[2,3],[3,4],[3,5],[5,4],[1,2],
+         [2,5],[2,4],[6,7],[3,6]  
+   - List (7,6,3,5,4,2,1) is a Hamiltonian path
+   - List (1,2,3,4,5,6,7) is not a Hamiltonian path
+*)
+
+open System
+printf "Enter the number of vertices n="
+let n=int(Console.ReadLine())
+printf "Enter the number of edges m="
+let m=int(Console.ReadLine())
+let adjacencyMatrix=Array2D.zeroCreate<int> (n+1) (n+1)
 for i in 1..m do 
-    printf "Dati x="
-    let x=int(System.Console.ReadLine())
-    printf "Dati y="
-    let y=int(System.Console.ReadLine())
-    matriceAdiacenta.[x,y] <- 1
-    matriceAdiacenta.[y,x] <- 1
+    printf "Enter x="
+    let x=int(Console.ReadLine())
+    printf "Enter y="
+    let y=int(Console.ReadLine())
+    adjacencyMatrix.[x,y] <- 1
+    adjacencyMatrix.[y,x] <- 1
+printfn "The adjacency matrix is :"
 for i in 1..n do
     for j in 1..n do
-        printf "%i  " matriceAdiacenta.[i,j]
+        printf "%i  " adjacencyMatrix.[i,j]
     printfn ""
 
-printf "Dati numarul de varfuri din lant nr="
-let nr=int(System.Console.ReadLine())
-let lant=[for i in 1..nr do 
-               yield(printf "lant[%i]=" i
-                     int(System.Console.ReadLine()))]
+printf "Enter the number of vertices from the list nr="
+let nr=int(Console.ReadLine())
+let path=[for i in 1..nr do 
+               yield(printf "path[%i]=" i
+                     int(Console.ReadLine()))]
 let rez=
     let rec loop i=
         match i<(nr-1) with
         | false -> i
-        | true -> match matriceAdiacenta.[lant.[i],lant.[i+1]]=1 with     
+        | true -> match adjacencyMatrix.[path.[i],path.[i+1]]=1 with     
                   | false -> i
                   | true -> loop (i+1)
     loop 0
 printfn "rez=%i" rez
-if rez<nr-1 then printfn "Lista nu este un lant in graf"
+if rez<nr-1 then printfn "The list is not a path in the graph."
     else
-        let distincte=List.distinct lant
-        if distincte.Length<n then printfn "Lantul nu contine toate nodurile grafului."
+        let distinct=List.distinct path
+        if distinct.Length<n then printfn "The path does not contain all vertices from the graph."
             else
-                printfn "Lantul contine toate nodurile grafului."
+                printfn "This is a Hamiltonian path."
 
