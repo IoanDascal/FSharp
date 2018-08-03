4ab441c63573b9463634f3413bc1f169802b8ffb
Nelu Dascal <idascal@computervoice.ro>
2018-08-03 16:59:17 +0300

    Added comments to var 31 sub II pr 5

--- var31subII_p5.fs
+++ var31subII_p5.fs
@@ -1,9 +1,14 @@
-printf "Dati n="
-let n=int(System.Console.ReadLine())
-let lista=[]
-let res=List.rev [for i in 1..n do     
-                      printf "Dati un cuvant:" 
-                      let cuvant=System.Console.ReadLine()
-                      if cuvant.[0]=cuvant.[cuvant.Length-1] then yield(cuvant)]
+(*
+    Write a function to insert in a list all words which have the same first and 
+last letter from a sentence. Store the words in reverse order of their occurrence.
+*)
 
-printfn "%A" res
+open System
+printf "Enter n="
+let n=int(Console.ReadLine())
+let listOfWords=List.rev [for i in 1..n do     
+                              printf "Enter a wordt:" 
+                              let word=Console.ReadLine()
+                              if word.[0]=word.[word.Length-1] then yield(word)]
+
+printfn "%A" listOfWords
