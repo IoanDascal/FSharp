f301016b10c5096c90762c7ac80b250211f0bda3
Nelu Dascal <idascal@computervoice.ro>
2018-10-05 10:02:32 +0300

    Added a Parser Combinator Example

--- ParserCombinatorsEx1.fs
+++ ParserCombinatorsEx1.fs
@@ -3,11 +3,10 @@
 *)
 
 
-type Result<'a>=
-    | Success of 'a*list<char>
-    | Failure of string
-type Parser<'a> = 
-    | Parser of (char list -> Result<'a*char list,string>)
+type Result<'TSuccess,'TFailure>=
+    | Success of 'TSuccess
+    | Failure of 'TFailure
+type Parser<'a> = Parser of (char list -> Result<'a*char list,string>)
 let stringToCharList str=
     List.ofSeq str
 let runParser parser inputChars=
@@ -16,15 +15,17 @@ let runParser parser inputChars=
 let expectChar expectedChar=
     let innerParser inputChars=
         match inputChars with
-        | c::remainingChars -> if c=expectedChar then Success (string c,remainingChars)
+        | c::remainingChars -> if c=expectedChar then Success (c,remainingChars)
                                else Failure (sprintf "Expected %c, got %c" expectedChar c) 
         | [] -> Failure (sprintf "Expected %c, reached end of input" expectedChar)
-    innerParser
+    Parser innerParser
 
-let orParse parser1 parser2 inputChars=
-    match parser1 inputChars with
-    | Success (c,remainingChars) -> Success (c,remainingChars)  
-    | Failure _-> parser2 inputChars
+let orParse parser1 parser2 =
+    let innerParser inputChars=
+        match runParser parser1 inputChars with
+        | Success result -> Success result 
+        | Failure _-> runParser parser2 inputChars
+    Parser innerParser
 let ( <|> )=orParse
 let choice parserList=
     List.reduce ( <|> ) parserList
@@ -33,19 +34,53 @@ let anyCharOf validChars=
     |> List.map expectChar
     |> choice
 
-
+(*
 stringToCharList "take" 
 |> anyCharOf ['a'..'m']
 |> printfn "%A"
+*)
 
-let andParse parser1 parser2 inputChars=
-    match parser1 inputChars with
-    | Failure msg -> Failure msg  
-    | Success (c1,remaining1) -> match parser2 remaining1 with
-                                 | Failure msg -> Failure msg  
-                                 | Success (c2,remaining2) -> Success (c1+c2,remaining2)
+let andParse parser1 parser2= 
+    let innerParser inputChars=
+        match runParser parser1 inputChars with
+        | Failure msg -> Failure msg  
+        | Success (c1,remaining1) -> match runParser parser2 remaining1 with
+                                     | Failure msg -> Failure msg  
+                                     | Success (c2,remaining2) -> Success ((c1,c2),remaining2)
+    Parser innerParser
 let ( .>>. ) = andParse
+let mapParser mapFunc parser=
+    let innerParser inputChars=
+        match runParser parser inputChars with
+        | Failure msg -> Failure msg  
+        | Success (result,remaining) -> Success (mapFunc result,remaining)
+    Parser innerParser
+let applyParser funcAsParser paramAsParser=
+    (funcAsParser .>>. paramAsParser)
+    |> mapParser (fun (f,x) -> f x)
+let ( <*> ) = applyParser
+let returnAsParser result=
+    let innerParser inputChars=
+        Success (result,inputChars)
+    Parser innerParser
+let liftToParser2 funcToLift paramAsParser1 paramAsParser2=
+    returnAsParser funcToLift <*> paramAsParser1 <*> paramAsParser2
+
+let rec sequenceParsers parserList=
+    let cons head rest = head :: rest
+    let consAsParser=liftToParser2 cons
+    match parserList with
+    | [] -> returnAsParser []
+    | parser::remainingParsers -> consAsParser parser (sequenceParsers remainingParsers)
+let charListAsString chars=
+    System.String (List.toArray chars)
+let expectString expectedString=
+    expectedString
+    |> stringToCharList
+    |> List.map expectChar
+    |> sequenceParsers
+    |> mapParser charListAsString
 
 stringToCharList "legat"
-|> (expectChar 'l' .>>. expectChar 'e' .>>. expectChar 'g')
+|> runParser (expectString "lega")
 |> printfn "%A"
