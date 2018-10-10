160408f33f8ab9a4d7dd967c91999c5181397f91
Nelu Dascal <idascal@computervoice.ro>
2018-10-10 10:18:14 +0300

    Added comments to var 32 sub III pr 4

--- var32subIII_p4.fs
+++ var32subIII_p4.fs
@@ -1,14 +1,21 @@
+(*
+    File nrVar324.txt contains an array of Int32 values.
+Write a program that print the lowest two numbers with two
+digits from the file. If there aren't at least two numbers
+with two digits the program should print 0.
+*)
 open System.IO
-let input=File.OpenText("nrVar324.txt")
-let citireLinie (isr:StreamReader)=
-    let sir=isr.ReadToEnd()
-    let sir=sir.Replace(System.Environment.NewLine," ")
-    let sir=sir.Split([|' '|])
-    sir
+let input=File.OpenText("C:/Users/Nelu/Desktop/fsvsc/FSharp/nrVar324.txt")
+let readLine (isr:StreamReader)=
+    let inputString=isr.ReadToEnd()
+    let inputString=inputString.Replace(System.Environment.NewLine," ")
+    let inputArray=inputString.Split([|' '|])
+    inputArray
 
-let numere=Array.map int (citireLinie input)
-let nr2Cifre=Array.filter (fun x -> abs x>=10 && abs x<100) numere
-let minim=Array.min nr2Cifre
-let nr2cif=Array.filter (fun x -> x<> minim) nr2Cifre
-let minim1=Array.min nr2cif
-printfn "%i  %i" minim minim1
+let numbers=Array.map int (readLine input)
+let numbersWithTwoDigits=Array.filter (fun x -> abs x>=10 && abs x<100) numbers
+if numbersWithTwoDigits.Length<2 then printfn "0"
+    else
+        let minimum=Array.min numbersWithTwoDigits
+        let nextMinimum=Array.minBy (fun x -> if x=minimum then System.Int32.MaxValue else x) numbersWithTwoDigits
+        printfn "%i  %i" minimum nextMinimum
