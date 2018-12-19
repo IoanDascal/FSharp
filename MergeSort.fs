bef92d383da14513f80830f4531f9da31e583a41
Nelu Dascal <idascal@computervoice.ro>
2018-12-19 15:33:30 +0200

    Added MergeSort Implementation

---
+++ MergeSort.fs
@@ -0,0 +1,8 @@
+(*
+    https://www.geeksforgeeks.org/merge-sort/
+    https://www.coursera.org/lecture/algorithms-part1/mergesort-ARWDq
+    
+*)
+
+let L=['a';'b';'c']
+printfn "%A" L.Tail.Tail.Head
