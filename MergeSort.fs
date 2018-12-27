92c7a071c960536b1d4b590cbb6d907f2577cbbf
Nelu Dascal <idascal@computervoice.ro>
2018-12-27 12:01:02 +0200

    Added changes to Merge Sort

--- MergeSort.fs
+++ MergeSort.fs
@@ -4,9 +4,8 @@
     https://stackoverflow.com/questions/35594171/merge-sort-for-f-sharp
 *)
 
-let x=[|3;6;9;13;25;36|]
-let y=[|1;7;11;27|]
-let merge (x:int array) (y:int array) =
+// For arrays
+let mergeArray (x:int array) (y:int array) =
     let rec mergeA x y cont=
         match (x,y) with
         | [||],b -> cont b
@@ -16,15 +15,14 @@ let merge (x:int array) (y:int array) =
                  else
                      mergeA a b.[1..] (fun r -> cont (Array.concat [[|b.[0]|];r]))
     mergeA x y id
-let res=merge x y
 
-let rec mergeSort (x:int array)=
+let mergeSortArray (x:int array)=
     let rec mergesort x cont=
         match x with
         | [||] -> cont [||]
         | [|a|] -> cont x
         | _ -> let mid=(Array.length x)/2
-               mergesort x.[..mid] (fun y -> mergesort x.[mid..] (fun z -> cont (merge y z)))
+               mergesort x.[..mid-1] (fun y -> mergesort x.[mid..] (fun z -> cont (merge y z)))
     mergesort x id
 
 // initialization function
@@ -33,37 +31,35 @@ let randFunc =
   rnd
 
 
-let randomArray = Array.init 100000 randFunc
-printfn "%A" randomArray
-let resA=mergeSort randomArray
-printfn "%A" resA
-printfn "%d" resA.[(Array.length resA)-2]
+let randomArray = Array.init 100 randFunc
+printfn "Unsorted array : %A" randomArray
+let resA=mergeSortArray randomArray
+printfn "Sorted array : %A" resA
 
 //For lists
 // create a random list 
-let randomList = List.init 100000 randFunc
-printfn "%A" randomList
+let randomList = List.init 100 randFunc
+printfn "Unsorted list : %A" randomList
 
-let merge xs ys = 
-    let rec mrg xs ys cont =
+let mergeList xs ys = 
+    let rec mergeL xs ys cont =
         match (xs, ys) with
         | ([], ys) -> cont ys
         | (xs, []) -> cont xs
         | (x::xs', y::ys') ->
             if x < y 
-            then mrg xs' ys (fun rs -> cont (x::rs))
-            else mrg xs ys' (fun rs -> cont (y::rs))
-    mrg xs ys id
-let mergeSort xs = 
-    let rec msort xs cont = 
+            then mergeL xs' ys (fun rs -> cont (x::rs))
+            else mergeL xs ys' (fun rs -> cont (y::rs))
+    mergeL xs ys id
+let mergeSortList xs = 
+    let rec mergesort xs cont = 
         match xs with
         | []    -> cont []
         | [x]   -> cont xs
         | _     -> 
             let mid = List.length xs / 2
             let (xs', xs'') = List.splitAt mid xs
-            msort xs' (fun ys' -> msort xs'' (fun ys'' -> cont (merge ys' ys'')))
-    msort xs id
-let resS=mergeSort randomList
-printfn "%A" resS
-printfn "%d" resS.[(List.length resS)-2]
+            mergesort xs' (fun ys' -> mergesort xs'' (fun ys'' -> cont (mergeList ys' ys'')))
+    mergesort xs id
+let resS=mergeSortList randomList
+printfn "Sorted list : %A" resS
