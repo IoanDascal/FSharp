84f08ee72e805bcb9a866307f4640b8072b97026
Nelu Dascal <idascal@computervoice.ro>
2019-01-04 13:45:49 +0200

    Added comments to bubble sort

--- BubbleSort.fs
+++ BubbleSort.fs
@@ -1,5 +1,6 @@
 (*
     https://www.geeksforgeeks.org/bubble-sort/
+    https://rosettacode.org/wiki/Sorting_algorithms/Bubble_sort#F.23
 *)
 
 open System
@@ -28,3 +29,16 @@ let bubbleSort (vector:int array)=
     
 let sortedArray=bubbleSort vector
 printfn "The sorted array is: %A" sortedArray
+
+//For lists
+let BubbleSort (lst : list<int>) = 
+    let rec sort accum rev lst =
+        match lst, rev with
+        | [], true -> accum |> List.rev
+        | [], false -> accum |> List.rev |> sort [] true
+        | x::y::tail, _ when x > y -> sort (y::accum) false (x::tail)
+        | head::tail, _ -> sort (head::accum) rev tail
+    sort [] true lst
+
+let res=BubbleSort [45;34;2;43;12]
+printfn "%A" res
