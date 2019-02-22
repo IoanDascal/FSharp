50bdc072ce4ddec30625211854a6565dc64f713f
Nelu Dascal <idascal@computervoice.ro>
2019-02-22 12:59:25 +0200

    Added Implementation Of Lee's Algorithm

---
+++ LEE_ex1.fs
@@ -0,0 +1,11 @@
+(*
+    https://www.geeksforgeeks.org/shortest-path-in-a-binary-maze/
+    http://yadiragarnicabonome.com/lee-algorithm-find-the-shortest-path/
+    
+    Shortest path in a Binary Maze
+
+    Given a MxN matrix where each element can either be 0 or 1. 
+We need to find the shortest path between a given source cell to 
+a destination cell. The path can only be created out of a cell if its value is 1.
+    Expected time complexity is O(MN).
+*)
