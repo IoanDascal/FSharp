39966d7097988eb73a63018fcb52945dcf9493c6
Nelu Dascal <idascal@computervoice.ro>
2019-03-08 15:40:29 +0200

    Added updates 8.03.2019

--- reflexionsEx1.fs
+++ reflexionsEx1.fs
@@ -1,12 +1,37 @@
 (*
     https://stackoverflow.com/questions/4470366/using-all-cases-of-union-type-f?noredirect=1&lq=1
+    https://stackoverflow.com/questions/35537160/parameterizing-extracting-discriminated-union-cases
+    https://stackoverflow.com/questions/21559497/create-discriminated-union-case-from-string?rq=1
 *)
 
 open System
+open Microsoft.FSharp.Reflection
+(*
+type Health =
+    | Healed
+    | Damaged
+    | Died
+    | Revived
+
+let health = Event<Health>()
+let pub    = health.Publish
+printfn "%A" pub
+let only msg pub = pub |> Observable.choose (function
+    | x when x = msg -> Some ()
+    | _              -> None
+)
+let healed  = pub |> only Healed
+let damaged = pub |> only Damaged
+let died    = pub |> only Died
+let revived = pub |> only Revived
+
+printfn "%A" healed
+*)
 
 type Beverage =
     | Coffee
     | Tea
+    | Beer
     static member ToStrings() =
         FSharp.Reflection.FSharpType.GetUnionCases( typeof<Beverage>) |> Array.map (fun info -> info.ToString())
     override self.ToString() =
@@ -15,18 +40,21 @@ type Beverage =
 let res=Beverage.ToStrings()
 printfn "%A" res
 
+let bev=[Coffee;Tea;Beer]
+for b in bev do printfn "%A" b
 type Shape =
     | Square
     | Circle
     | Triangle
     | Other
     static member ToStrings() =
-        FSharp.Reflection.FSharpType.GetUnionCases( typeof<Shape>) |> Array.map (fun info -> info.ToString())
+        FSharp.Reflection.FSharpType.GetUnionCases( typeof<Shape>) //|> Array.map (fun info -> info.ToString())
     override self.ToString() =
         sprintf "%A" self
 
 let res1=Shape.ToStrings()
 printfn "%A" res1
+
 
 type Color =
     | Black
@@ -51,7 +79,7 @@ let colorCases = Color.ToStrings()
 let numberOfRows = 5
 let numberOfColumns = 3
 let boardOfRelevantPossibilities = Array2D.init<ColoredShape> numberOfRows numberOfColumns (fun x y -> {Shape = Other; Color = Black})
-
+printfn "%A" boardOfRelevantPossibilities
 let rand = Random()
 
 for shapeCase in shapeCases do
